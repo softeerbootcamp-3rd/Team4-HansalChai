94d1b25d9b2ccece37fc64345bffb4e5f54b7952
sean2337 <sean2337@naver.com>
2024-02-04 00:03:10 +0900

    feat: GlobalStyle적용 및 font적용
    GlobalStyle적용을 적용하고 브라우저마다의 다른 style을 리셋하였고 폰트를 적용해주었습니다 🙌🙌

--- haul-fe/src/App.jsx
+++ haul-fe/src/App.jsx
@@ -1,7 +1,13 @@
 import Router from "./routes/Router.jsx";
+import GlobalStyle from "./styles/GlobalStyle.jsx";
 
 function App() {
-  return <Router />;
+  return (
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
+  );
 }
 
 export default App;
