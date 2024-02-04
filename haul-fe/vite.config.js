a2fdb4e12ad73a019deb2cec2d96b56e61539c41
sean2337 <sean2337@naver.com>
2024-02-04 22:30:45 +0900

    fix: SVG함수 버그 수정
    SVG함수로 인해 버그있던걸 함수를 제거함으로서 해결

--- haul-fe/vite.config.js
+++ haul-fe/vite.config.js
@@ -1,11 +1,9 @@
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
-import svgReact from "vite-plugin-svg-react";
 
 export default defineConfig({
   plugins: [
-    svgReact(),
     react(),
     VitePWA({
       registerType: "autoUpdate",
