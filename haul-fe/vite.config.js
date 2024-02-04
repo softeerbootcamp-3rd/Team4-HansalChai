7088e03f8fba9973314ed005a79345556707fdab
sean2337 <sean2337@naver.com>
2024-02-04 17:48:24 +0900

    feat: DetailInfo 컴포넌트를 위한 설치
    구글맵 API, SVG플러그인을 설치하였습니다.

--- haul-fe/vite.config.js
+++ haul-fe/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
+import svgReact from "vite-plugin-svg-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    svgReact(),
     react(),
     VitePWA({
       registerType: "autoUpdate",
