a3db4c74bd34af1fd28969b676b0dc3df4952826
sean2337 <sean2337@naver.com>
2024-02-04 13:30:57 +0900

    feat: Typhography 공용 컴포넌트 생성
    Typhography공용 컴포넌트를 생성했고, 자주 사용하는 폰트 형태를 theme에 정리해두어 이를 props로 받아 간단하게 구현할 수 있게 하였습니다

--- haul-fe/src/styles/theme/Theme.jsx
+++ haul-fe/src/styles/theme/Theme.jsx
@@ -13,11 +13,66 @@ const colors = {
 };
 
 const font = {
-  mainTitle: `
-        font-family: 'pretendard-bold';
-        font-size: 26px;
-        line-height: 31px;
+  bold36: `
+        font-family: 'bold';
+        font-size: 36px;
       `,
+  bold24: `
+        font-family: 'bold';
+        font-size: 24px;
+      `,
+  bold20: `
+      font-family: 'bold';
+      font-size: 20px;
+    `,
+  bold16: `
+      font-family: 'bold';
+      font-size: 16px;
+    `,
+  bold14: `
+    font-family: 'bold';
+    font-size: 14px;
+  `,
+  bold12: `
+    font-family: 'bold';
+    font-size: 12px;
+  `,
+  semiBold20: `
+    font-family: 'semiBold';
+    font-size: 20px;
+  `,
+  semiBold16: `
+    font-family: 'semiBold';
+    font-size: 16px;
+  `,
+  semiBold12: `
+    font-family: 'semiBold';
+    font-size: 12px;
+  `,
+  medium16: `
+    font-family: 'medium';
+    font-size: 16px;
+  `,
+  medium12: `
+    font-family: 'medium';
+    font-size: 12px;
+  `,
+  medium10: `
+    font-family: 'medium';
+    font-size: 10px;
+  `,
+  regular16: `
+    font-family: 'regular';
+    font-size: 16px;
+  `,
+  regular14: `
+    font-family: 'regular';
+    font-size: 14px;
+  `,
+  regular12: `
+    font-family: 'regular';
+    font-size: 12px;
+  `,
 };
 
 const flex = {
