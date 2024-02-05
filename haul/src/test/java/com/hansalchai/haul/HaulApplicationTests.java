144331adfa0feb7b880ebfe9034aa55a3e01ef77
jiaeYoon <yoon529@sookmyung.ac.kr>
2024-02-05 14:03:13 +0900

    feat : inital setting

---
+++ haul/src/test/java/com/hansalchai/haul/HaulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansalchai.haul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
