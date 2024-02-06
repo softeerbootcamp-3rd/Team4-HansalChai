26c28732b670fdb6438727a78f01ec47d3ac9c8c
sean2337 <sean2337@naver.com>
2024-02-06 11:12:35 +0900

    refactor: 재랜더링을 최소화하는 로직 추가
    useState 대신 useRef를 input의 value로 가져감에 따라 두 값이 모두 채워져 있는지는 useState 하나로 관리하게 하여 랜더링을 최소화함

--- haul-fe/src/views/Login/Login.jsx
+++ haul-fe/src/views/Login/Login.jsx
@@ -3,12 +3,25 @@ import Typography from "../../components/Typhography/Typhography";
 import Flex from "../../components/Flex/Flex";
 import Margin from "../../components/Margin/Margin";
 import Input from "../../components/Input/Input";
-import { useRef } from "react";
+import BottomButton from "../../components/Button/BottomButton";
+import FixedCenterBox from "../../components/FixedBox/FixedCenterBox";
+import styled from "styled-components";
+import { useRef, useState } from "react";
+import { FaArrowRight } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
+const GoSignUpBtn = styled.button`
+  width: auto;
+  display: flex;
+  line-height: 14px;
+`;
 
 const Login = () => {
+  const navigate = useNavigate();
+
   const tel = useRef("");
   const password = useRef("");
-
+  const [isButtonDisabled, setButtonDisabled] = useState(true);
   return (
     <MobileLayout>
       <Margin height="40px" />
@@ -18,23 +31,59 @@ const Login = () => {
       <Margin height="83px" />
       <Typography font="bold24">로그인 하기</Typography>
       <Margin height="10px" />
-      <Typography font="bold16" color="subColor">
-        또는 회원가입하기
-      </Typography>
+      <GoSignUpBtn
+        onClick={() => {
+          navigate("/signUp");
+        }}
+      >
+        <Typography font="bold16" color="subColor">
+          또는 회원가입하기
+        </Typography>
+        <FaArrowRight
+          style={{
+            fontSize: "14px",
+            color: "#596FB7",
+            marginLeft: "3px",
+          }}
+        />
+      </GoSignUpBtn>
+
       <Margin height="36px" />
       <Input
         size="big"
         type="tel"
         placeholder="Phone Number "
-        inputValue={tel}
+        onChange={({ target: { value } }) => {
+          tel.current = value;
+          let checkIsButtonDisabled = !(tel.current && password.current);
+          if (checkIsButtonDisabled !== isButtonDisabled) {
+            setButtonDisabled(checkIsButtonDisabled);
+          }
+        }}
       />
       <Margin height="20px" />
       <Input
         size="big"
         type="password"
         placeholder="Password "
-        inputValue={password}
+        onChange={({ target: { value } }) => {
+          password.current = value;
+          let checkIsButtonDisabled = !(tel.current && password.current);
+          if (checkIsButtonDisabled !== isButtonDisabled) {
+            setButtonDisabled(checkIsButtonDisabled);
+          }
+        }}
       />
+
+      <FixedCenterBox bottom="30px">
+        <BottomButton role="main" disabled={isButtonDisabled}>
+          로그인하기
+        </BottomButton>
+        <Margin height="10px" />
+        <BottomButton role="sub" disabled={false}>
+          비회원으로 접속하기
+        </BottomButton>
+      </FixedCenterBox>
     </MobileLayout>
   );
 };
