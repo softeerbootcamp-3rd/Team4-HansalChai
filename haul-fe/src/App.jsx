d8040bd22e28fbbbe0195ffd52fdb7a62450fc8a
sean2337 <sean2337@naver.com>
2024-02-06 22:08:37 +0900

    feature: Store1차 구현
    Store객체를 설계 후에 개발하여 앱 전체에 useContext를 통해 모든 컴포넌트에서 받을 수 있게 구현함

--- haul-fe/src/App.jsx
+++ haul-fe/src/App.jsx
@@ -4,13 +4,16 @@ import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme/Theme.jsx";
 import "./assets/fonts/fonts.css";
 import ToastRoot from "./components/Toast/ToastRoot.jsx";
+import { StoreProvider } from "./\bstore/store.jsx";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Router />
-      <ToastRoot />
+      <StoreProvider>
+        <GlobalStyle />
+        <Router />
+        <ToastRoot />
+      </StoreProvider>
     </ThemeProvider>
   );
 }
