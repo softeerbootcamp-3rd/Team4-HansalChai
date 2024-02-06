f12285bbeff1b5bd5c3626e923a711a1eee14bd0
sean2337 <sean2337@naver.com>
2024-02-06 13:50:06 +0900

    feature: GuestLogin페이지 Router 연결
    GuestLogin페이지를 import해서 /guestLogin url에 연결해주었습니다

--- haul-fe/src/routes/Router.jsx
+++ haul-fe/src/routes/Router.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import TestView from "../views/TestView";
 import Splash from "../views/Splash/Splash";
 import Login from "../views/Login/Login";
+import GuestLogin from "../views/GuestLogin/GuestLogin";
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Splash />} />
-      <Route path="/Login" element={<Login />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/guestLogin" element={<GuestLogin />} />
       <Route path="/Test" element={<TestView />} />
     </Routes>
   </BrowserRouter>
