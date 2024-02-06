fb8c9a11267c189562eecd06c424e101f8b1f689
sean2337 <sean2337@naver.com>
2024-02-06 15:33:09 +0900

    feature: Typhography에 flex추가
    Typhography 안에 어떤 요소가 있는 걸 고려해서 정중앙 정렬을 위한 Flex속성을 추가하였습니다

--- haul-fe/src/components/Typhography/Typhography.jsx
+++ haul-fe/src/components/Typhography/Typhography.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 const Typo = styled.p`
+  display: flex;
+  align-items: center;
   color: ${(props) =>
     props.color ? props.theme.colors[props.color] : props.theme.colors.black};
   ${(props) => props.font && props.theme.font[props.font]};
