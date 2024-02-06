b3e7383b0e9ae51d268518d2cce7cf2ee94406ed
sjmjys954646 <sjmjys954646@gmail.com>
2024-02-06 16:58:54 +0900

    chore : 필요없는 주석 제거

--- haul-be/src/main/java/com/hansalchai/haul/common/exceptions/BadRequestException.java
+++ haul-be/src/main/java/com/hansalchai/haul/common/exceptions/BadRequestException.java
@@ -4,7 +4,6 @@ import com.hansalchai.haul.common.utils.ErrorCode;
 
 import lombok.Getter;
 
-// HTTP 상태 코드 400 Bad Request : 잘못된 요청
 @Getter
 public class BadRequestException extends RuntimeException {
 	private final ErrorCode code;
