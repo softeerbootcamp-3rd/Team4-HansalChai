288ff878d2b0aef53bdd3678546311277dcb8d6a
sean2337 <sean2337@naver.com>
2024-02-06 17:53:09 +0900

    feature: 운송수단 페이지 라우터 적용
    운송수단 페이지 라우터 적용를 적용하였습니다. url은 기존에 정해진 haulRequest/choiceTransport로 설정하였습니다

--- haul-fe/src/routes/Router.jsx
+++ haul-fe/src/routes/Router.jsx
@@ -4,6 +4,7 @@ import Splash from "../views/Splash/Splash";
 import Login from "../views/Login/Login";
 import GuestLogin from "../views/GuestLogin/GuestLogin";
 import SignUP from "../views/SignUp/SignUp";
+import ChoiceTransport from "../views/ChoiceTransport/ChoiceTransport";
 
 const Router = () => (
   <BrowserRouter>
@@ -13,6 +14,10 @@ const Router = () => (
       <Route path="/guestLogin" element={<GuestLogin />} />
       <Route path="/signUp" element={<SignUP />} />
       <Route path="/Test" element={<TestView />} />
+      <Route
+        path="/haulRequest/choiceTransport"
+        element={<ChoiceTransport />}
+      />
     </Routes>
   </BrowserRouter>
 );
