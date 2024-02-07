7cde8e6e8501ef06dab372ea758fdcfe32a8c31f
sean2337 <sean2337@naver.com>
2024-02-07 13:01:05 +0900

    feature: 날짜 선택 페이지 구현
    날짜 선택페이지를 위해 달력을 직접 구현하였고, 이를 Store와 연결함에 따라 날짜 선택 완료시 Store에 저장하게 로직 추가

---
+++ haul-fe/src/views/ChoiceDate/ChoiceDate.jsx
@@ -0,0 +1,62 @@
+import MobileLayout from "../../components/MobileLayout/MobileLayout.jsx";
+import Header from "../../components/Header/Header.jsx";
+import Margin from "../../components/Margin/Margin.jsx";
+import Typography from "../../components/Typhography/Typhography.jsx";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { reservationStore } from "../../\bstore/reservationStore.jsx";
+import { isEmptyString } from "../../utils/helper.js";
+import Calendar from "./components/Calendar.jsx";
+import FixedCenterBox from "../../components/FixedBox/FixedCenterBox.jsx";
+import BottomButton from "../../components/Button/BottomButton.jsx";
+
+const ChoiceDate = () => {
+  const [selectedDay, setSelectedDay] = useState(new Date());
+  const navigation = useNavigate();
+  const { setReservationDate } = useContext(reservationStore);
+  const {
+    state: { transportType },
+  } = useContext(reservationStore);
+
+  useEffect(() => {
+    if (isEmptyString(transportType)) {
+      navigation(-1);
+    }
+  }, []);
+
+  const DateFormChange = (date) => {
+    return date.getFullYear() + "." + date.getMonth() + "." + date.getDate();
+  };
+
+  return (
+    <MobileLayout>
+      <Margin height="10px" />
+      <Header>
+        HAUL<span style={{ color: "#596FB7" }}>.</span>
+      </Header>
+      <Margin height="24px" />
+      <Typography font="bold24">
+        <span style={{ color: "#596FB7" }}>{transportType}</span>을
+        선택하셨군요.
+      </Typography>
+      <Margin height="4px" />
+      <Typography font="bold24">언제 찾아뵈면 될까요?</Typography>
+      <Margin height="60px" />
+      <Calendar selectedDay={selectedDay} setSelectedDay={setSelectedDay} />
+
+      <FixedCenterBox bottom="20px">
+        <BottomButton
+          role="main"
+          disabled={!selectedDay}
+          onClick={() => {
+            setReservationDate(DateFormChange(selectedDay));
+            navigation("/haulRequest/choiceTime");
+          }}
+        >
+          선택 완료
+        </BottomButton>
+      </FixedCenterBox>
+    </MobileLayout>
+  );
+};
+export default ChoiceDate;
