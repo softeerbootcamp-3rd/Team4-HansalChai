91d78dcb116febb40f52320a72b3498605384997
Pransinia <trullerster@gmail.com>
2024-02-07 11:02:06 +0900

    feature: Route에 더보기 페이지 추가

--- haul-fe/src/routes/Router.jsx
+++ haul-fe/src/routes/Router.jsx
@@ -4,6 +4,7 @@ import Splash from "../views/Splash/Splash";
 import Login from "../views/Login/Login";
 import GuestLogin from "../views/GuestLogin/GuestLogin";
 import SignUP from "../views/SignUp/SignUp";
+import More from "../views/More/More";
 
 const Router = () => (
   <BrowserRouter>
@@ -12,6 +13,7 @@ const Router = () => (
       <Route path="/login" element={<Login />} />
       <Route path="/guestLogin" element={<GuestLogin />} />
       <Route path="/signUp" element={<SignUP />} />
+      <Route path="/more" element={<More />} />
       <Route path="/Test" element={<TestView />} />
     </Routes>
   </BrowserRouter>
