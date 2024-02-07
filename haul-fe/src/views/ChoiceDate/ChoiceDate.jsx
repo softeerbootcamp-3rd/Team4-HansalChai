af9dc5ea471c485b69190928c65efe06efbf575d
sean2337 <sean2337@naver.com>
2024-02-07 23:10:08 +0900

    refactor: ChoiceDate 페이지 리팩토링 작업
    ChoiceDate 페이지 리팩토링 작업을 진행하여 코드를 직관적으로 보일 수 있게 변경하였습니다

--- haul-fe/src/views/ChoiceDate/ChoiceDate.jsx
+++ haul-fe/src/views/ChoiceDate/ChoiceDate.jsx
@@ -1,27 +1,28 @@
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { reservationStore } from "../../store/reservationStore.jsx";
 import MobileLayout from "../../components/MobileLayout/MobileLayout.jsx";
 import Header from "../../components/Header/Header.jsx";
 import Margin from "../../components/Margin/Margin.jsx";
 import Typography from "../../components/Typhography/Typhography.jsx";
-import { useContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { reservationStore } from "../../store/reservationStore.jsx";
-import { isEmptyString } from "../../utils/helper.js";
+import Typography_Span from "../../components/Typhography/Typhography_Span.jsx";
 import Calendar from "./components/Calendar.jsx";
 import FixedCenterBox from "../../components/FixedBox/FixedCenterBox.jsx";
 import BottomButton from "../../components/Button/BottomButton.jsx";
+import { isEmptyString } from "../../utils/helper.js";
 import { UrlMap } from "../../data/GlobalVariable.js";
 
 const ChoiceDate = () => {
-  const [selectedDay, setSelectedDay] = useState(new Date());
   const navigation = useNavigate();
-  const { setReservationDate } = useContext(reservationStore);
+  const [selectedDay, setSelectedDay] = useState(new Date());
   const {
+    setReservationDate,
     state: { transportType },
   } = useContext(reservationStore);
 
   useEffect(() => {
     if (isEmptyString(transportType)) {
-      navigation(-1);
+      navigation(UrlMap.choiceTranportTypeUrl);
     }
   }, []);
 
@@ -35,11 +36,12 @@ const ChoiceDate = () => {
     <MobileLayout>
       <Margin height="10px" />
       <Header>
-        HAUL<span style={{ color: "#596FB7" }}>.</span>
+        HAUL
+        <Typography_Span color="subColor">.</Typography_Span>
       </Header>
       <Margin height="24px" />
       <Typography font="bold24">
-        <span style={{ color: "#596FB7" }}>{transportType}</span>을
+        <Typography_Span color="subColor">{transportType}</Typography_Span>을
         선택하셨군요.
       </Typography>
       <Margin height="4px" />
