592a6c4ea829fd2c40159aceb83ecb8558a41001
Pransinia <trullerster@gmail.com>
2024-02-09 20:52:19 +0900

    style: 약관 상세 페이지 하단 여백 및 스크롤바 삭제

--- haul-fe/src/views/Contract/Contract.jsx
+++ haul-fe/src/views/Contract/Contract.jsx
@@ -15,13 +15,18 @@ const contractType = {
 
 const ContractView = styled.pre`
   width: 100%;
-  height: calc(100vh - 70px - 24px - 32px - 83px - 30px);
+  height: calc(100vh - 10px - 24px - 32px - 83px);
   white-space: pre-wrap; /* css-3 */
   white-space: -moz-pre-wrap; /* Mozilla, since 1999 */
   white-space: -pre-wrap; /* Opera 4-6 */
   white-space: -o-pre-wrap; /* Opera 7 */
   word-wrap: break-word; /* Internet Explorer 5.5+ */
   overflow-y: scroll;
+  -ms-overflow-style: none; /* 인터넷 익스플로러 */
+  scrollbar-width: none; /* 파이어폭스 */
+  ::-webkit-scrollbar {
+    display: none; /* 크롬, 사파리, 오페라, 엣지 */
+  }
   ${({ theme }) => theme.font.medium12};
 `;
 
