67e2e6e20ce2d521fb61a21077e73a7aee4d0971
sean2337 <sean2337@naver.com>
2024-02-10 19:04:03 +0900

    feature: Input에 단위 넣을 수 있게 기능 추가
    Input에 단위를 넣을 수 있게 기능을 추가하고, Input글자 정렬을 선택할 수 있게 기능을 추가했습니다

--- haul-fe/src/components/Input/Input.jsx
+++ haul-fe/src/components/Input/Input.jsx
@@ -1,30 +1,61 @@
 import styled from "styled-components";
 
-const InputSection = styled.input`
+const InputWrapper = styled.div`
+  display: flex;
+  align-items: center;
   width: 100%;
   height: ${(props) => (props.size === "big" ? "56px" : "38px")};
   background-color: ${(props) => props.theme.colors.inputGray};
+  border-radius: 8px;
+  border: 1px solid #e1e1e1;
+`;
+
+const InputSection = styled.input`
+  flex-grow: 1;
   ${(props) =>
     props.size === "big"
       ? props.theme.font["regular16"]
       : props.theme.font["regular14"]};
   color: ${(props) => props.theme.colors.black};
-  padding: 0 20px;
-  border-radius: 8px;
-  border: 1px solid #e1e1e1;
+  padding-left: 20px;
+  padding-right: ${(props) => (props.textAlign === "right" ? "5px" : "20px")};
+  background-color: transparent;
+  border: none;
+  text-align: ${(props) => (props.textAlign === "right" ? "right" : "left")};
 `;
 
-//useRef의 value를 받기로 약속됨.
-const Input = ({ type, size, placeholder, onChange, value, readOnly }) => {
+const Unit = styled.span`
+  padding-right: 20px;
+  color: ${(props) => props.theme.colors.black};
+  ${(props) =>
+    props.size === "big"
+      ? props.theme.font["regular16"]
+      : props.theme.font["regular14"]};
+`;
+
+const Input = ({
+  type,
+  size,
+  placeholder,
+  onChange,
+  value,
+  readOnly,
+  unit,
+  textAlign,
+}) => {
   return (
-    <InputSection
-      value={value}
-      type={type}
-      size={size}
-      placeholder={placeholder}
-      onChange={onChange}
-      readOnly={readOnly}
-    />
+    <InputWrapper size={size}>
+      <InputSection
+        value={value}
+        type={type}
+        size={size}
+        placeholder={placeholder}
+        onChange={onChange}
+        readOnly={readOnly}
+        textAlign={textAlign}
+      />
+      {unit && <Unit size={size}>{unit}</Unit>}
+    </InputWrapper>
   );
 };
 
