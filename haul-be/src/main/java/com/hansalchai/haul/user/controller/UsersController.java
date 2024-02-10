76b15218bf74b5e2a83f3b27ab4646f943f326b2
jiaeYoon <yoon529@sookmyung.ac.kr>
2024-02-10 22:54:05 +0900

    chore : api url 수정
    url만 보고도 api임을 알 수 있게 하기위해, 서버와 클라이언트가 함께 제공될 때 구분하기 위해 /api/v1을 붙여 url을 수정했다.

--- haul-be/src/main/java/com/hansalchai/haul/user/controller/UsersController.java
+++ haul-be/src/main/java/com/hansalchai/haul/user/controller/UsersController.java
@@ -20,7 +20,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
-@RequestMapping("/users")
+@RequestMapping("api/v1/users")
 @RestController
 public class UsersController {
 
