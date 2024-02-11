2e2e5e7c2e7a463e320452145a1dfd528e50881f
sean2337 <sean2337@naver.com>
2024-02-11 12:24:31 +0900

    feature: 카카오맵을 통한 출발, 목적지 경로 표시 컴포넌트 생성
    카카오맵을 통해 출발지, 목적지 주소를 입력하면 경로가 표시되는 컴포넌트를 개발하였습니다

--- haul-fe/src/components/DetailInfo/RouteMap.jsx
+++ haul-fe/src/components/DetailInfo/RouteMap.jsx
@@ -1,52 +1,117 @@
-import React, { useState } from "react";
-import {
-  GoogleMap,
-  LoadScript,
-  DirectionsRenderer,
-} from "@react-google-maps/api";
+import React, { useEffect } from "react";
 
 const apiKey = import.meta.env.VITE_MAP_KEY;
+const restApiKey = import.meta.env.VITE_KAKAO_MAP_REST_KEY;
 
-const containerStyle = {
-  width: "100%",
-  height: "227px",
-  borderRadius: "10px",
-};
+// 지도, 출발지, 도착지 위도를 찍으면 지도에 경로를 그려주는 함수
+async function getCarDirection({ kakaoMap, srcCoordinate, dstCoordinate }) {
+  const REST_API_KEY = restApiKey;
+  const url = "https://apis-navi.kakaomobility.com/v1/directions";
+
+  const origin = `${srcCoordinate.lng},${srcCoordinate.lat}`;
+  const destination = `${dstCoordinate.lng},${dstCoordinate.lat}`;
+  const headers = {
+    Authorization: `KakaoAK ${REST_API_KEY}`,
+    "Content-Type": "application/json",
+  };
+  const queryParams = new URLSearchParams({
+    origin: origin,
+    destination: destination,
+  });
+
+  const requestUrl = `${url}?${queryParams}`; // 파라미터까지 포함된 전체 URL
+
+  try {
+    const response = await fetch(requestUrl, {
+      method: "GET",
+      headers: headers,
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+
+    const linePath = [];
+    data.routes[0].sections[0].roads.forEach((router) => {
+      router.vertexes.forEach((vertex, index) => {
+        if (index % 2 === 0) {
+          linePath.push(
+            new kakao.maps.LatLng(
+              router.vertexes[index + 1],
+              router.vertexes[index]
+            )
+          );
+        }
+      });
+    });
+
+    var bounds = new kakao.maps.LatLngBounds();
+    for (let i = 0; i < linePath.length; i++) {
+      bounds.extend(linePath[i]);
+    }
+    kakaoMap.setBounds(bounds);
+
+    var polyline = new kakao.maps.Polyline({
+      path: linePath,
+      strokeWeight: 5,
+      strokeColor: "#446EDA",
+      strokeOpacity: 0.7,
+      strokeStyle: "solid",
+    });
+    polyline.setMap(kakaoMap);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
 
 const RouteMap = ({ origin, destination }) => {
-  const [directions, setDirections] = useState(null);
-
-  const calculateRoute = () => {
-    const directionsService = new window.google.maps.DirectionsService();
-    const request = {
-      origin,
-      destination,
-      travelMode: window.google.maps.TravelMode.DRIVING,
+  useEffect(() => {
+    const mapContainer = document.getElementById("map");
+    const mapOptions = {
+      center: new kakao.maps.LatLng(origin.lat, origin.lng),
+      level: 10,
     };
 
-    directionsService.route(request, (result, status) => {
-      if (status === window.google.maps.DirectionsStatus.OK) {
-        setDirections(result);
-      } else {
-        console.error("경로 계산 실패:", status);
-      }
+    const kakaoMap = new kakao.maps.Map(mapContainer, mapOptions);
+    const originPosition = new kakao.maps.LatLng(origin.lat, origin.lng);
+    const destinationPosition = new kakao.maps.LatLng(
+      destination.lat,
+      destination.lng
+    );
+
+    // 출발지, 도착지 마커 생성
+    let originMarker = new kakao.maps.Marker({
+      position: originPosition,
     });
-  };
+
+    let destinationMaker = new kakao.maps.Marker({
+      position: destinationPosition,
+    });
+
+    originMarker.setMap(kakaoMap);
+    destinationMaker.setMap(kakaoMap);
+
+    //경로 생성
+    getCarDirection({
+      kakaoMap: kakaoMap,
+      srcCoordinate: origin,
+      dstCoordinate: destination,
+    });
+  }, []);
 
   return (
-    <LoadScript googleMapsApiKey={apiKey} onLoad={calculateRoute}>
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={origin}
-        zoom={14}
-        options={{
-          disableDefaultUI: true,
+    <>
+      <div
+        id="map"
+        style={{
+          width: "100%",
+          height: "227px",
         }}
-      >
-        {directions && <DirectionsRenderer directions={directions} />}
-      </GoogleMap>
-    </LoadScript>
+      ></div>
+    </>
   );
 };
 
-export default React.memo(RouteMap);
+export default RouteMap;
