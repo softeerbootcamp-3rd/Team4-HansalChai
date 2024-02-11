26d5c8f6fa9b84303e2b26982385d559a4fe043c
sean2337 <sean2337@naver.com>
2024-02-11 22:07:43 +0900

    feature: isPhoneNumber함수 생성
    인자로 받은 문자열이 전화번호인지 확인하는 isPhoneNumber함수를 생성하여 helper.js에 두었습니다

--- haul-fe/src/utils/helper.js
+++ haul-fe/src/utils/helper.js
@@ -2,10 +2,17 @@ export const isEmptyString = (value) => {
   return value.length === 0;
 };
 
+// 문자열이 이메일 형식인지 확인해주는 함수
 export const checkEmail = (email) => {
   const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/;
   return emailRegex.test(email);
 };
+
+// 문자열이 전화번호 형식인지 확인해주는 함수
+export function isPhoneNumber(input) {
+  const regPhone = /^(01[016789]|02|[0-9]{3})\d{7,8}$/;
+  return regPhone.test(input);
+}
 
 // 2022-02-08이라는 문자열을 넣으면 Date객체로 만들어주는 함수
 export function stringToDateObject(str) {
