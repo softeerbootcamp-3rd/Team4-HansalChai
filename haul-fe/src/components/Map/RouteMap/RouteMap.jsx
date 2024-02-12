9751c437a50ac9dcb93e7b689934e7fabf025fea
sean2337 <sean2337@naver.com>
2024-02-12 23:36:53 +0900

    fix: 지도 표시 안되는 문제 해결

--- haul-fe/src/components/Map/RouteMap/RouteMap.jsx
+++ haul-fe/src/components/Map/RouteMap/RouteMap.jsx
@@ -3,7 +3,6 @@ import { useEffect } from "react";
 const restApiKey = import.meta.env.VITE_KAKAO_MAP_REST_KEY;
 
 const loadKakaoMaps = drawdirection => {
-  if (window.kakao !== undefined) return;
   const script = document.createElement("script");
   script.type = "text/javascript";
   script.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${
