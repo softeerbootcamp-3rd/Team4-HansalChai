42d244103681e58bc4373c915a6b211c9b1ab545
jiaeYoon <yoon529@sookmyung.ac.kr>
2024-02-12 22:31:29 +0900

    test : 컨트롤러 계층의 로그인 테스트 코드 작성

--- haul-be/src/test/java/com/hansalchai/haul/user/controller/UsersControllerTest.java
+++ haul-be/src/test/java/com/hansalchai/haul/user/controller/UsersControllerTest.java
@@ -13,14 +13,23 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hansalchai.haul.common.auth.constants.Role;
 import com.hansalchai.haul.user.dto.CustomerSignUpDto;
+import com.hansalchai.haul.user.dto.UserLoginDto;
+import com.hansalchai.haul.user.entity.Users;
+import com.hansalchai.haul.user.repository.UsersRepository;
+
+import jakarta.transaction.Transactional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 class UsersControllerTest {
 
 	@Autowired MockMvc mockMvc;
 	@Autowired ObjectMapper objectMapper;
+
+	@Autowired UsersRepository usersRepository;
 
 	@Test
 	@DisplayName("회원가입 테스트")
@@ -36,6 +45,33 @@ class UsersControllerTest {
 				.contentType(MediaType.APPLICATION_JSON)
 				.accept(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk())
+			.andDo(print());
+	}
+
+	@Test
+	@DisplayName("로그인 테스트")
+	void signIn() throws Exception {
+
+		//given
+		Users user = Users.builder()
+			.name("haul")
+			.tel("01012341234")
+			.password("password")
+			.email("haul@gmail.com")
+			.role(Role.CUSTOMER)
+			.build();
+		usersRepository.save(user);
+
+		//when, then
+		String content = objectMapper.writeValueAsString(new UserLoginDto("01012341234", "password"));
+
+		mockMvc.perform(post("/api/v1/users/sign-in")
+				.content(content)
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.data.accessToken").exists())
+			.andExpect(jsonPath("$.data.refreshToken").exists())
 			.andDo(print());
 	}
 }
