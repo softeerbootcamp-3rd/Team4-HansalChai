2208f499102196b03b2c0b23af4ca0656037d693
sean2337 <sean2337@naver.com>
2024-02-13 17:14:03 +0900

    feature: 에러 메시지 추가
    비밀번호 8자리 이상을 위한 에러 메시지를 추가했습니다

--- haul-fe/src/data/GlobalVariable.js
+++ haul-fe/src/data/GlobalVariable.js
@@ -24,5 +24,6 @@ export const ErrorMessageMap = {
   InvalidEmailformat: "이메일 형식이 아닙니다.",
   NotSamePassword: "비밀번호가 일치하지 않습니다.",
   IsNotNumber: "숫자만 입력 가능합니다.",
-  IsNotPositiveNumber: "0 이하는 입력 불가능합니다."
+  IsNotPositiveNumber: "0 이하는 입력 불가능합니다.",
+  IsNotMinPasswordCount: "8자리 이상의 비밀번호만 가능합니다."
 };
