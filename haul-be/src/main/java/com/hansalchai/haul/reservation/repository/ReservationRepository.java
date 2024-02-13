c14e774b34314cdececb09c44d0e5dc6cf28db13
sjmjys954646 <sjmjys954646@gmail.com>
2024-02-13 18:39:56 +0900

    feat : reservation find ByCustomerId repository
    예약 데이터 찾아오기

--- haul-be/src/main/java/com/hansalchai/haul/reservation/repository/ReservationRepository.java
+++ haul-be/src/main/java/com/hansalchai/haul/reservation/repository/ReservationRepository.java
@@ -1,9 +1,12 @@
 package com.hansalchai.haul.reservation.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hansalchai.haul.reservation.entity.Reservation;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
+	Page<Reservation> findByCustomerId(Long userId, Pageable pageable);
 }
