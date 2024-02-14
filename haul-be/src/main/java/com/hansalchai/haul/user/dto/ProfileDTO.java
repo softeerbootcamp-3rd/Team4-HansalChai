d15dd8646ce43523725e2ef68e33bb2c7a8305fd
sjmjys954646 <sjmjys954646@gmail.com>
2024-02-14 11:52:56 +0900

    feat : add noargsconstructor to dto
    dto에 noArgsConstructor를 붙임

--- haul-be/src/main/java/com/hansalchai/haul/user/dto/ProfileDTO.java
+++ haul-be/src/main/java/com/hansalchai/haul/user/dto/ProfileDTO.java
@@ -5,15 +5,17 @@ import com.hansalchai.haul.user.entity.Users;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class ProfileDTO {
 	@NotNull(message = "이름은 null일 수 없습니다.")
-	private final String name;
+	private String name;
 	@NotNull(message = "비밀번호는 null일 수 없습니다.")
-	private final String email;
+	private String email;
 	@NotNull(message = "전화번호(아이디)는 null일 수 없습니다.")
-	private final String tel;
+	private String tel;
 
 	@Builder
 	public ProfileDTO(Users user){
--- haul-be/src/main/java/com/hansalchai/haul/user/dto/ProfileUpdateDTO.java
+++ haul-be/src/main/java/com/hansalchai/haul/user/dto/ProfileUpdateDTO.java
@@ -4,17 +4,19 @@ import com.hansalchai.haul.user.entity.Users;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class ProfileUpdateDTO {
 	@NotNull(message = "이름은 null일 수 없습니다.")
-	private final String name;
+	private String name;
 	@NotNull(message = "비밀번호는 null일 수 없습니다.")
-	private final String email;
+	private String email;
 	@NotNull(message = "전화번호(아이디)는 null일 수 없습니다.")
-	private final String password;
+	private String password;
 
 	public ProfileUpdateDTO(String name, String email, String password) {
 		this.name = name;
