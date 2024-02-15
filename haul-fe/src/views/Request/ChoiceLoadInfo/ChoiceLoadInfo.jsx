070c8cfae2806a51621aa73add14f24b74b13b38
sean2337 <sean2337@naver.com>
2024-02-15 17:35:02 +0900

    feature: TransportType 변수 따로 저장
    TransportType에 대한 배열을 Global 변수로써 저장해두고 사용합니다

--- haul-fe/src/views/Request/ChoiceLoadInfo/ChoiceLoadInfo.jsx
+++ haul-fe/src/views/Request/ChoiceLoadInfo/ChoiceLoadInfo.jsx
@@ -13,7 +13,11 @@ import NavigationBar from "../../../components/NavigationBar/NavigationBar.jsx";
 import BottomButton from "../../../components/Button/BottomButton.jsx";
 import FixedCenterBox from "../../../components/FixedBox/FixedCenterBox.jsx";
 import ToastMaker from "../../../components/Toast/ToastMaker.jsx";
-import { UrlMap, ErrorMessageMap } from "../../../data/GlobalVariable.js";
+import {
+  UrlMap,
+  ErrorMessageMap,
+  TransportTypeArr
+} from "../../../data/GlobalVariable.js";
 import { isNumber, isPositiveNumber } from "../../../utils/helper.js";
 import { getIsMember } from "../../../utils/localStorage.js";
 import { memberReservationFun } from "../../../repository/reservationRepository.js";
@@ -53,6 +57,7 @@ const ChoiceLoadInfo = () => {
     setRoadInfo,
     getReservationState,
     state: {
+      transportType,
       dstAddress,
       cargoWeight,
       cargoWidth,
@@ -96,6 +101,11 @@ const ChoiceLoadInfo = () => {
   }
 
   async function SumbitStore() {
+    const cargoWeightNum = Number(inCargoWeight.current);
+    const cargoWidthNum = Number(inCargoWidth.current);
+    const cargoLengthNum = Number(inCargoLength.current);
+    const cargoHeightNum = Number(inCargoHeight.current);
+
     if (
       !isNumber(inCargoWeight.current) ||
       !isNumber(inCargoWidth.current) ||
@@ -118,11 +128,50 @@ const ChoiceLoadInfo = () => {
       return;
     }
 
+    function checkWeightToTypeFun(checkTransportType, weight) {
+      for (const transportInfo of TransportTypeArr) {
+        if (transportInfo.transportType === checkTransportType) {
+          if (weight > transportInfo.maxLoad * 1000) {
+            return transportInfo.maxLoad;
+          } else {
+            return 0;
+          }
+        }
+      }
+      return 0;
+    }
+
+    const checkWeightToType = checkWeightToTypeFun(
+      transportType,
+      cargoWeightNum
+    );
+    //transportType에 대한 분류
+    if (checkWeightToType > 0) {
+      ToastMaker({
+        type: "info",
+        children: `현재 선택하신 운송종류는 ${checkWeightToType}톤까지 실으실 수 있습니다.`
+      });
+      return;
+    }
+
+    //최대 너비, 높이, 길이에 대한 분류
+    if (
+      cargoWidthNum > 1000 ||
+      cargoLengthNum > 1000 ||
+      cargoHeightNum > 1000
+    ) {
+      ToastMaker({
+        type: "info",
+        children: `Haul은 1000cm미만의 크기를 지원합니다.`
+      });
+      return;
+    }
+
     setRoadInfo({
-      cargoWeight: Number(inCargoWeight.current),
-      cargoWidth: Number(inCargoWidth.current),
-      cargoLength: Number(inCargoLength.current),
-      cargoHeight: Number(inCargoHeight.current),
+      cargoWeight: cargoWeightNum,
+      cargoWidth: cargoWidthNum,
+      cargoLength: cargoLengthNum,
+      cargoHeight: cargoHeightNum,
       specialNotes: inSpecialNotes
     });
 
