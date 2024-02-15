26286843fab80c0be9a4e2f8bdaec83f8a862b57
Pransinia <trullerster@gmail.com>
2024-02-15 15:48:14 +0900

    chore: 기사 앱 초기 설정

---
+++ haul-driver-fe/vite.config.js
@@ -0,0 +1,45 @@
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        icons: [
+          {
+            src: "/icon_x192.png",
+            type: "image/png",
+            sizes: "192x192"
+          },
+          {
+            src: "/icon_x192_maskable.png",
+            type: "image/png",
+            sizes: "192x192",
+            purpose: "maskable"
+          },
+          {
+            src: "/icon_x512.png",
+            type: "image/png",
+            sizes: "512x512"
+          },
+          {
+            src: "/icon_x512_maskable.png",
+            type: "image/png",
+            sizes: "512x512",
+            purpose: "maskable"
+          }
+        ],
+        background_color: "#000B49",
+        theme_color: "#000B49"
+      }
+    })
+  ]
+});
