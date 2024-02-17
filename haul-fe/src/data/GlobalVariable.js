189ede9564ba3b21f64d07a66f6646ecdd72b337
Pransinia <trullerster@gmail.com>
2024-02-17 14:13:02 +0900

    refactor: 사용자 정보 관련 fetch 함수의 오류메세지를 Globalvariable.js로 이동

--- haul-fe/src/data/GlobalVariable.js
+++ haul-fe/src/data/GlobalVariable.js
@@ -33,7 +33,10 @@ export const ErrorMessageMap = {
   NotSamePassword: "비밀번호가 일치하지 않습니다.",
   IsNotNumber: "숫자만 입력 가능합니다.",
   IsNotPositiveNumber: "0 이하는 입력 불가능합니다.",
-  IsNotMinPasswordCount: "8자리 이상의 비밀번호만 가능합니다."
+  IsNotMinPasswordCount: "8자리 이상의 비밀번호만 가능합니다.",
+  NoUserFound: "사용자 정보를 찾을 수 없었어요.",
+  ChangePasswordFailed: "비밀번호 변경에 실패했어요.",
+  TryLater: "잠시 후 다시 시도해주세요."
 };
 
 export const TransportTypeArr = [
