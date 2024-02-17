dc57ea594702f2b47eaccf8d9519995958f03d67
Pransinia <trullerster@gmail.com>
2024-02-17 20:58:56 +0900

    refactor: 무한 리스트의 인자 추가 및 변경
    fetcher에 리스트 허용. 단, fetcherIndex가 유효한 인덱스여야 함
    baseURL 추가. 각 요소 클릭 시 baseURL/id로 이동

--- haul-driver-fe/src/components/InfiniteList/InfiniteList.jsx
+++ haul-driver-fe/src/components/InfiniteList/InfiniteList.jsx
@@ -66,7 +66,7 @@ function useIntersectionObserver(callback) {
   return [observe, unobserve, disconnect];
 }
 
-const InfiniteList = ({ fetcher }) => {
+const InfiniteList = ({ fetcher, baseURL, fetcherIndex = null }) => {
   const [isEnd, setIsEnd] = useState(false); //데이터를 모두 불러왔으면 end를 트리거 시키기 위한 state
   const realEndRef = useRef(false); //리스트를 모두 불러왔는지 확인하기 위한 ref
   const endRef = useRef(null); //마지막 요소를 참조하기 위한 ref
@@ -135,10 +135,11 @@ const InfiniteList = ({ fetcher }) => {
     <ListFrame>
       {reservationList.map((data, index) => (
         <div key={`reserv${index}`}>
-          <Link to={`/check/detail/${data.id}`} key={`reserv${index}`}>
+          <Link to={`${baseURL}/${data.id}`} key={`reserv${index}`}>
             <SummaryItemBox
-              model={data.car}
               status={data.status}
+              src={data.src}
+              dst={data.dst}
               time={data.datetime}
               fee={data.cost}
             />
