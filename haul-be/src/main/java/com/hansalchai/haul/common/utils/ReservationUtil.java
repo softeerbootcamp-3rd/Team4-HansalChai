6b1afd59644dac29e40db1bf3597dc1ce83a09a2
jiaeYoon <yoon529@sookmyung.ac.kr>
2024-02-17 17:40:30 +0900

    feat : 예약 날짜의 문자열 형식 변환 기능 구현
    LocalDate는 '-'로 년,월,일을 구분한다. 2024-02-12 이를 2024.02.12처럼 '.'으로 구분하도록 날짜 형식 변환 메서드를 구현했다.

--- haul-be/src/main/java/com/hansalchai/haul/common/utils/ReservationUtil.java
+++ haul-be/src/main/java/com/hansalchai/haul/common/utils/ReservationUtil.java
@@ -2,6 +2,7 @@ package com.hansalchai.haul.common.utils;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -9,8 +10,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ReservationUtil {
 
+	private static final DateTimeFormatter dateOutputFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+
 	public static String dateTimeToString(LocalDate date, LocalTime time) {
-		return date.toString() + " " + time.toString();
+		return convertDateFormat(date) + " " + time.toString();
+	}
+
+	// 문자열 형식 변환 : 2024-02-12 -> 2024.02.12
+	private static String convertDateFormat(LocalDate date) {
+		return date.format(dateOutputFormatter);
 	}
 
 	public static int cutCost(int fee) {
