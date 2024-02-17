3d0571eec74fc832d8ac50b6744f66359d72e9d6
Pransinia <trullerster@gmail.com>
2024-02-17 21:56:31 +0900

    fix: 리스트 페이지의 Header 문구 수정

--- haul-driver-fe/src/views/ScheduleCheck/ScheduleCheckList/ScheduleCheckList.jsx
+++ haul-driver-fe/src/views/ScheduleCheck/ScheduleCheckList/ScheduleCheckList.jsx
@@ -4,25 +4,27 @@ import Margin from "../../../components/Margin/Margin.jsx";
 import MobileLayout from "../../../components/MobileLayout/MobileLayout.jsx";
 import Flex from "../../../components/Flex/Flex.jsx";
 import NavigationBar from "../../../components/NavigationBar/NavigationBar.jsx";
-import Typography from "../../../components/Typhography/Typhography.jsx";
 import InfiniteList from "../../../components/InfiniteList/InfiniteList.jsx";
 import UnderBar from "../../../components/UnderBar/UnderBar.jsx";
 import TabBar from "../../../components/TabBar/TabBar.jsx";
 import { getUserSummaryList } from "../../../repository/checkRepository.jsx";
+import TypographySpan from "../../../components/Typhography/TyphographySpan.jsx";
 
 
 //TODO: API가 운송상태를 구별하여 가져올 수 있게 변경된 후 리스트 수정할 것!
 const ScheduleCheckList = () => {
   const [selectedStatus, setSelectedStatus] = useState(0);
+  const driverName = "시현";
 
   const statusList = ["운송 전", "운송 중", "운송 완료"];
 
   return (
     <MobileLayout>
       <Header home={false} back={false}>
-        <Typography font={"semiBold24"} color={"mainColor"}>
-          예약 확인
-        </Typography>
+        <TypographySpan font="semiBold24" color="subColor">
+          {driverName}기사
+          <TypographySpan>님의 일정</TypographySpan>
+        </TypographySpan>
       </Header>
       <Margin height="32px" />
       <TabBar
