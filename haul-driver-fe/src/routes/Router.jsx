b1a2158c1764b35597d36fde9f11bb070d0101e6
sean2337 <sean2337@naver.com>
2024-02-17 16:43:00 +0900

    feature: 기사버전 일정잡기 상세 페이지 퍼블리싱 작업 완료
    기사버전의 일정잡기를 위한 상세 페이지 퍼블리싱 개발을 완료했습니다

--- haul-driver-fe/src/routes/Router.jsx
+++ haul-driver-fe/src/routes/Router.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
 import Login from "../views/Login/Login";
 import Splash from "../views/Splash/Splash";
-import Complete from "../views/Complete/Complete";
+import Complete from "../views/ScheduleCreate/Complete/Complete";
+import ScheduleCreateDetail from "../views/ScheduleCreate/ScheduleCreateDetail/ScheduleCreateDetail";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -20,6 +22,10 @@ const Router = () => (
       <Route path="/" element={<Splash />} />
       <Route path="/login" element={<Login />} />
       <Route path="/complete" element={<Complete />} />
+      
+      <Route path="/schedule-create" element={<Outlet />}>
+        <Route path=":reservationId" element={<ScheduleCreateDetail />} />
+      </Route>
     </Routes>
   </BrowserRouter>
 );
