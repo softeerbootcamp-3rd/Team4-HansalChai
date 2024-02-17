4252fac8c984bf4397b565a3eff1ce08325830df
jiaeYoon <yoon529@sookmyung.ac.kr>
2024-02-17 21:47:07 +0900

    feat : 주소 요약 메서드 보완
    기존 메서드는 '서울특별시 강남구 강남대로 지하396'라는 주소에 대해 서울특별시 강남구로 변환했다. 이를 서울 '강남구'로 변환하도록 수정했다. 또한 특별자치도, 특별자치시, 광역시에 대한 변환 또한 추가했다.
    '세종특별자치시 국책연구원2로 18'의 경우 두번째 토큰이 길어 ui가 깨질 가능성이 있다. 따라서 '로'가 포함된 경우, 첫번째 토큰만 반환하도록 처리했다.

--- haul-be/src/main/java/com/hansalchai/haul/common/utils/AddressUtil.java
+++ haul-be/src/main/java/com/hansalchai/haul/common/utils/AddressUtil.java
@@ -6,9 +6,31 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AddressUtil {
 
-	// '서울특별시 강남구 강남대로 지하396' -> '서울시 강남구'로 변환
+	// 주소 요약 메서드
 	public static String toSimpleAddress(String address) {
 		String[] tokens = address.split(" ");
-		return tokens[0] + " " + tokens[1];
+
+		StringBuilder simpleAddress = new StringBuilder();
+
+		// 제주특별자치도 -> 제주도
+		if (tokens[0].equals("제주특별자치도")) {
+			simpleAddress.append("제주도");
+		}
+		//서울특별시 -> 서울, 세종특별자치시 -> 세종, 전북특별자치도 -> 전북
+		else {
+			simpleAddress.append(
+				tokens[0]
+					.replace("특별시", "")
+					.replace("광역시", "")
+					.replace("특별자치시", "")
+					.replace("특별자치도", ""));
+		}
+
+		//세종특별자치시 국책연구원2로 18 -> 세종
+		if (!tokens[1].contains("로")) {
+			simpleAddress.append(" " + tokens[1]);
+		}
+
+		return simpleAddress.toString();
 	}
 }
