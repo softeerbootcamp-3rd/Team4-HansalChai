366522e44477c06ec4c96bc5518d3779a88f3584
Pransinia <trullerster@gmail.com>
2024-02-17 21:39:46 +0900

    add: 더보기 메뉴 페이지 추가

---
+++ haul-driver-fe/src/views/More/List/More.jsx
@@ -0,0 +1,53 @@
+import Header from "../../../components/Header/Header.jsx";
+import Margin from "../../../components/Margin/Margin.jsx";
+import MobileLayout from "../../../components/MobileLayout/MobileLayout.jsx";
+import Flex from "../../../components/Flex/Flex.jsx";
+import { IoIosArrowForward as ArrowIcon } from "react-icons/io";
+import NavigationBar from "../../../components/NavigationBar/NavigationBar.jsx";
+import styled from "styled-components";
+import Typography from "../../../components/Typhography/Typhography.jsx";
+import UnderBar from "../../../components/UnderBar/UnderBar.jsx";
+import { useNavigate } from "react-router-dom";
+
+const ListItem = styled.div`
+  width: 100%;
+  height: 52px;
+  cursor: pointer;
+  ${({ theme }) => theme.flex.flexBetweenAlignCenter};
+`;
+
+const More = () => {
+  const navigate = useNavigate();
+
+  const clickUserInfo = () => {
+    navigate("/more/user-info");
+  };
+
+  const clickTerms = () => {
+    navigate("/more/terms");
+  };
+
+  return (
+    <MobileLayout>
+      <Header home={false} back={false}>
+        <Typography font={"semiBold24"}>더보기</Typography>
+      </Header>
+      <Margin height="32px" />
+      <Flex kind="flexColumn">
+        <ListItem id={"more__user-info"} onClick={clickUserInfo}>
+          <Typography font={"medium16"}>내 정보</Typography>
+          <ArrowIcon />
+        </ListItem>
+        <UnderBar />
+        <ListItem id={"more__terms"} onClick={clickTerms}>
+          <Typography font={"medium16"}>약관 및 정책</Typography>
+          <ArrowIcon />
+        </ListItem>
+        <UnderBar />
+      </Flex>
+      <NavigationBar selected="more" />
+    </MobileLayout>
+  );
+};
+
+export default More;
