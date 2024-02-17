28931b8e6115c37d93cba40d53b7c4ac56363567
Pransinia <trullerster@gmail.com>
2024-02-17 21:44:05 +0900

    add: 추가된 페이지를 라우터에 등록
    더보기 메뉴 페이지, 리스트 페이지가 추가됨

--- haul-driver-fe/src/routes/Router.jsx
+++ haul-driver-fe/src/routes/Router.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
-import { Outlet } from "react-router-dom";
 import Login from "../views/Login/Login.jsx";
-import Check from "../views/Check/List/Check.jsx";
-import CheckDetail from "../views/Check/CheckDetail/CheckDetail.jsx";
 import Splash from "../views/Splash/Splash";
 import Complete from "../views/ScheduleCreate/Complete/Complete";
 import ScheduleCreateDetail from "../views/ScheduleCreate/ScheduleCreateDetail/ScheduleCreateDetail";
+import ScheduleCreateList from "../views/ScheduleCreate/ScheduleCreateList/ScheduleCreateList.jsx";
 import ScheduleCheckDetail from "../views/ScheduleCheck/ScheduleCheckDetail/ScheduleCheckDetail";
+import ScheduleCheckList from "../views/ScheduleCheck/ScheduleCheckList/ScheduleCheckList.jsx";
+import More from "../views/More/List/More.jsx";
+import Terms from "../views/More/Terms/Terms.jsx";
+import Contract from "../views/More/Terms/Contract/Contract.jsx";
+import UserInfo from "../views/More/UserInfo/UserInfo.jsx";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -26,13 +29,29 @@ const Router = () => (
       <Route path="/login" element={<Login />} />
 
       <Route path="/schedule-create" element={<Outlet />}>
+        <Route path="" element={<ScheduleCreateList />}/>
         <Route path=":reservationId" element={<ScheduleCreateDetail />} />
         <Route path="complete" element={<Complete />} />
       </Route>
 
       <Route path="/schedule-check" element={<Outlet />}>
+        <Route path="" element={<ScheduleCheckList />} />
         <Route path=":reservationId" element={<ScheduleCheckDetail />} />
-        <Route path="" element={<Check />} />
+      </Route>
+
+      <Route path="/more" element={<Outlet />}>
+        <Route path="" element={<More />} />
+        <Route path="user-info" element={<UserInfo />} />
+        <Route path="terms" element={<Outlet />}>
+          <Route path="" element={<Terms />} />
+          <Route path="services" element={<Contract type={"services"} />} />
+          <Route path="privacy" element={<Contract type={"privacy"} />} />
+          <Route
+            path="transportation"
+            element={<Contract type={"transportation"} />}
+          />
+          <Route path="location" element={<Contract type={"location"} />} />
+        </Route>
       </Route>
     </Routes>
   </BrowserRouter>
