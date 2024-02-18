d80e57989b5e4857832991123d7440489c342979
jiaeYoon <yoon529@sookmyung.ac.kr>
2024-02-18 17:46:16 +0900

    add : 토큰, 유저 관련 에러 코드 정의
    토큰, 유저와 관련되어 발생할 수 있는 에러를 코드를 부여해 정의했다. 예외처리 및 응답 전송 시 사용된다.

--- haul-be/src/main/java/com/hansalchai/haul/common/utils/ErrorCode.java
+++ haul-be/src/main/java/com/hansalchai/haul/common/utils/ErrorCode.java
@@ -3,9 +3,7 @@ package com.hansalchai.haul.common.utils;
 import static org.springframework.http.HttpStatus.*;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
@@ -13,7 +11,16 @@ public enum ErrorCode {
 	/* 400 BAD REQUEST */
 	// general
 	NO_ID(BAD_REQUEST, "40001", "존재하지 않는 id 입니다"),
-	MethodArgumentNotValidException(BAD_REQUEST,"20002002","MethodArgumentNotValidException");
+	MethodArgumentNotValidException(BAD_REQUEST,"20002002","MethodArgumentNotValidException"),
+
+	// 토큰, 유저
+	TOKEN_NOT_FOUND(UNAUTHORIZED, "2001", "요청 헤더에 토큰이 없습니다."),
+	INVALID_TOKEN(UNAUTHORIZED, "2002", "토큰 유효성 검증에 실패했습니다."),
+	EXPIRED_TOKEN(UNAUTHORIZED, "2003", "토큰이 만료되었습니다."),
+	ACCOUNT_ALREADY_EXISTS(CONFLICT, "2004", "이미 가입된 전화번호입니다."),
+	UNREGISTERED_USER_ID(UNAUTHORIZED, "2005", "존재하지 않는 아이디입니다."),
+	USER_NOT_FOUND(NOT_FOUND, "2006", "사용자를 찾을 수 없습니다."),
+	INCORRECT_PASSWORD(UNAUTHORIZED, "2007", "비밀번호가 일치하지 않습니다.");
 
 	private final HttpStatus status;
 	private final String code;
