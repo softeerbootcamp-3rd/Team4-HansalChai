8b74d7c5c19d17364e9863dacf243952a230076b
sean2337 <sean2337@naver.com>
2024-02-18 12:46:22 +0900

    feature: 로그인 로직 생성 및 로그인 필수 페이지에 관련 로직 추가
    로그인 로직을 생성하였고, 로그인이 필요한 페이지에 로그인이 안이루어져있다면 로그인 페이지로 이동하게 수정하였습니다

--- haul-driver-fe/src/routes/Router.jsx
+++ haul-driver-fe/src/routes/Router.jsx
@@ -1,5 +1,11 @@
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
-import { useLocation } from "react-router-dom";
+import {
+  BrowserRouter,
+  Outlet,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate
+} from "react-router-dom";
 import { useEffect } from "react";
 import Login from "../views/Login/Login.jsx";
 import Splash from "../views/Splash/Splash";
@@ -12,6 +18,8 @@ import More from "../views/More/List/More.jsx";
 import Terms from "../views/More/Terms/Terms.jsx";
 import Contract from "../views/More/Terms/Contract/Contract.jsx";
 import UserInfo from "../views/More/UserInfo/UserInfo.jsx";
+import { getAccessToken } from "../utils/localStorage.js";
+import { UrlMap } from "../data/GlobalVariable.js";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -28,33 +36,53 @@ const Router = () => (
       <Route path="/" element={<Splash />} />
       <Route path="/login" element={<Login />} />
 
-      <Route path="/schedule-create" element={<Outlet />}>
-        <Route path="" element={<ScheduleCreateList />} />
-        <Route path=":reservationId" element={<ScheduleCreateDetail />} />
-        <Route path="complete" element={<Complete />} />
-      </Route>
+      <Route element={<LoginCheckRoute />}>
+        <Route path="/schedule-create" element={<Outlet />}>
+          <Route path="" element={<ScheduleCreateList />} />
+          <Route path=":reservationId" element={<ScheduleCreateDetail />} />
+          <Route path="complete" element={<Complete />} />
+        </Route>
 
-      <Route path="/schedule-check" element={<Outlet />}>
-        <Route path="" element={<ScheduleCheckList />} />
-        <Route path=":reservationId" element={<ScheduleCheckDetail />} />
-      </Route>
+        <Route path="/schedule-check" element={<Outlet />}>
+          <Route path="" element={<ScheduleCheckList />} />
+          <Route path=":reservationId" element={<ScheduleCheckDetail />} />
+        </Route>
 
-      <Route path="/more" element={<Outlet />}>
-        <Route path="" element={<More />} />
-        <Route path="user-info" element={<UserInfo />} />
-        <Route path="terms" element={<Outlet />}>
-          <Route path="" element={<Terms />} />
-          <Route path="services" element={<Contract type={"services"} />} />
-          <Route path="privacy" element={<Contract type={"privacy"} />} />
-          <Route
-            path="transportation"
-            element={<Contract type={"transportation"} />}
-          />
-          <Route path="location" element={<Contract type={"location"} />} />
+        <Route path="/more" element={<Outlet />}>
+          <Route path="" element={<More />} />
+          <Route path="user-info" element={<UserInfo />} />
+          <Route path="terms" element={<Outlet />}>
+            <Route path="" element={<Terms />} />
+            <Route path="services" element={<Contract type={"services"} />} />
+            <Route path="privacy" element={<Contract type={"privacy"} />} />
+            <Route
+              path="transportation"
+              element={<Contract type={"transportation"} />}
+            />
+            <Route path="location" element={<Contract type={"location"} />} />
+          </Route>
         </Route>
       </Route>
     </Routes>
   </BrowserRouter>
 );
+
+const LoginCheckRoute = () => {
+  const navigate = useNavigate();
+  const isLoggedIn = checkLogin();
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate(UrlMap.loginPageUrl, { replace: true });
+    }
+  }, [isLoggedIn, navigate]);
+
+  return isLoggedIn ? <Outlet /> : null;
+};
+
+function checkLogin() {
+  const userAccessToken = getAccessToken();
+  return userAccessToken !== null;
+}
 
 export default Router;
--- haul-driver-fe/src/views/Login/Login.jsx
+++ haul-driver-fe/src/views/Login/Login.jsx
@@ -7,11 +7,12 @@ import Margin from "../../components/Margin/Margin.jsx";
 import Input from "../../components/Input/Input.jsx";
 import BottomButton from "../../components/Button/BottomButton.jsx";
 import FixedCenterBox from "../../components/FixedBox/FixedCenterBox.jsx";
-import { ErrorMessageMap } from "../../data/GlobalVariable.js";
+import { ErrorMessageMap, UrlMap } from "../../data/GlobalVariable.js";
 import { isPhoneNumber, deleteDash, isValueArr } from "../../utils/helper.js";
 import ToastMaker from "../../components/Toast/ToastMaker.jsx";
 import { useNavigate } from "react-router-dom";
-import { UrlMap } from "../../data/GlobalVariable.js";
+import { loginFun } from "../../repository/userRepository.jsx";
+import { setAccessToken, setRefreshToken } from "../../utils/localStorage.js";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -30,14 +31,25 @@ const Login = () => {
   }
 
   //로그인 버튼 클릭 시 실행 함수
-  function loginBtnFun() {
+  async function loginBtnFun() {
+    tel.current = deleteDash(tel.current);
     //전화번호 형식 예외처리
     if (!isPhoneNumber(tel.current)) {
       ToastMaker({ type: "error", children: ErrorMessageMap.InvalidTelformat });
       return;
     }
-    tel.current = deleteDash(tel.current);
-    navigate(UrlMap.scheduleCreateDetailPageUrl);
+    const { success, data, message } = await loginFun({
+      tel: tel.current,
+      password: password.current
+    });
+    if (success) {
+      setAccessToken(data.data.accessToken);
+      setRefreshToken(data.data.refreshToken);
+      navigate(UrlMap.scheduleCreateDetailPageUrl);
+    } else {
+      //FIXME: 로그인 실패 예외처리
+      ToastMaker({ type: "error", children: message });
+    }
   }
 
   return (
