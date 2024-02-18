278959e6c4186dc24c4a614167f089a2dbbe3e29
sjmjys954646 <sjmjys954646@gmail.com>
2024-02-18 10:57:23 +0900

    feat : add cargo detail, transportstatus, detail address to Order
    회의후 필요한 내용 추가

--- haul-be/src/main/java/com/hansalchai/haul/order/dto/OrderResponse.java
+++ haul-be/src/main/java/com/hansalchai/haul/order/dto/OrderResponse.java
@@ -6,6 +6,9 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
+import org.hibernate.validator.constraints.Range;
+
+import com.hansalchai.haul.reservation.constants.TransportStatus;
 import com.hansalchai.haul.reservation.entity.Reservation;
 
 import jakarta.validation.constraints.NotNull;
@@ -56,10 +59,13 @@ public class OrderResponse {
 		private final UserDTO user;
 		private final SourceDTO src;
 		private final DestinationDTO dst;
+		private final CargoDTO cargo;
 		@NotNull(message = "비용은 Null 일 수 없다.")
 		private final int cost;
 		@NotNull(message = "걸리는시간은 Null 일 수 없다.")
 		private final double requiredTime;
+		@NotNull(message = "현재 상태는 Null 일 수 없다.")
+		private final String transportStatus;
 
 		@Getter
 		@Builder
@@ -75,6 +81,8 @@ public class OrderResponse {
 			private String name;
 			@NotNull(message = "출발지 주소는 Null 일 수 없다.")
 			private String address;
+			@NotNull(message = "출발지 주소는 Null 일 수 없다.")
+			private String detailAddress;
 			@NotNull(message = "출발지 위도는 Null 일 수 없다.")
 			private double latitude;
 			@NotNull(message = "출발지 경도는 Null 일 수 없다.")
@@ -88,10 +96,25 @@ public class OrderResponse {
 			private String name;
 			@NotNull(message = "출발지 주소는 Null 일 수 없다.")
 			private String address;
+			@NotNull(message = "출발지 주소는 Null 일 수 없다.")
+			private String detailAddress;
 			@NotNull(message = "출발지 위도는 Null 일 수 없다.")
 			private double latitude;
 			@NotNull(message = "출발지 경도는 Null 일 수 없다.")
 			private double longitude;
+		}
+
+		@Getter
+		@Builder
+		public static class CargoDTO{
+			@NotNull(message = "화물 가로는 Null 일 수 없다.")
+			private int width;
+			@NotNull(message = "화물 세로는 Null 일 수 없다.")
+			private int length;
+			@NotNull(message = "화물 높이는 Null 일 수 없다.")
+			private int height;
+			@NotNull(message = "화물 무게는 Null 일 수 없다.")
+			private int weight;
 		}
 
 		@Builder
@@ -103,17 +126,26 @@ public class OrderResponse {
 			this.src = SourceDTO.builder()
 				.name(reservation.getSource().getName())
 				.address(reservation.getSource().getAddress())
+				.detailAddress(reservation.getSource().getDetailAddress())
 				.latitude(reservation.getSource().getLatitude())
 				.longitude(reservation.getSource().getLongitude())
 				.build();
 			this.dst = DestinationDTO.builder()
 				.name(reservation.getDestination().getName())
 				.address(reservation.getDestination().getAddress())
+				.detailAddress(reservation.getDestination().getDetailAddress())
 				.latitude(reservation.getDestination().getLatitude())
 				.longitude(reservation.getDestination().getLongitude())
 				.build();
+			this.cargo = CargoDTO.builder()
+				.width(reservation.getCargo().getWidth())
+				.length(reservation.getCargo().getLength())
+				.height(reservation.getCargo().getHeight())
+				.weight(reservation.getCargo().getWeight())
+				.build();
 			this.cost = costCut(reservation.getTransport().getFee());
 			this.requiredTime = reservation.getTransport().getRequiredTime();
+			this.transportStatus = TransportStatus.getCode(reservation.getTransport().getTransportStatus());
 		}
 		public int costCut(int fee){
 			if(fee < 10000)
