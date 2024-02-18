563fc8c7b443c00ac33de054e6a80f0bc4480f6e
Pransinia <trullerster@gmail.com>
2024-02-19 03:32:13 +0900

    fix: listStatus에 따라 fetch하지 않는 현상 수정

--- haul-driver-fe/src/components/InfiniteList/InfiniteList.jsx
+++ haul-driver-fe/src/components/InfiniteList/InfiniteList.jsx
@@ -38,18 +38,21 @@ const ListEnd = () => {
 
 //IntersectionObserver를 훅으로 만들어서 사용
 function useIntersectionObserver(callback) {
-  const observer = useRef(
-    new IntersectionObserver(
+  //useEffect를 통해 컴포넌트 생성시에만 observer를 생성하고, 컴포넌트가 사라질 때에만 observer를 제거함
+  const observer = useRef();
+  useEffect(() => {
+    observer.current = new IntersectionObserver(
       (entries, observer) => {
         entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            callback();
-          }
+          if (!entry.isIntersecting) return;
+          observer.unobserve(entry.target);
+          callback();
         });
       },
       { threshold: 0 }
-    )
-  );
+    );
+    return () => observer.current.disconnect();
+  }, []);
 
   const observe = element => {
     observer.current.observe(element);
@@ -66,52 +69,74 @@ function useIntersectionObserver(callback) {
   return [observe, unobserve, disconnect];
 }
 
-const InfiniteList = ({ fetcher, baseURL, listStatus = null }) => {
-  const [isEnd, setIsEnd] = useState(false); //데이터를 모두 불러왔으면 end를 트리거 시키기 위한 state
+//TODO: 상태 최대한 줄일 것!
+const InfiniteList = ({ fetcher, baseURL, listStatus }) => {
+  const [isEnd, setIsEnd] = useState(() => false); //데이터를 모두 불러왔으면 end를 트리거 시키기 위한 state
   const realEndRef = useRef(false); //리스트를 모두 불러왔는지 확인하기 위한 ref
   const endRef = useRef(null); //마지막 요소를 참조하기 위한 ref
-  const [page, setPage] = useState(0); //현재 불러와진 최종 페이지
-  const [isLoading, setIsLoading] = useState(true); //데이터를 불러오는 중이면 True
+  const page = useRef(0); //현재 불러와진 최종 페이지
+  const isLoading = useRef(true); //데이터를 불러오는 중이면 True
   const [reservationList, setReservationList] = useState([]); //현재 불러와진 예약 리스트
 
   //IntersectionObserver에 마지막 요소가 잡히면 페이지를 1 증가시킴
   const [observe, unobserve, disconnect] = useIntersectionObserver(() => {
     if (realEndRef.current) return;
-    setPage(prev => prev + 1);
+
+    page.current += 1;
+    () =>
+      (async () => {
+        if (realEndRef.current) return;
+
+        isLoading.current = true;
+        (async () => {
+          await runFetcher();
+        })();
+
+        isLoading.current = false;
+      })();
   });
 
-  //페이지가 바뀔 때마다 새로운 데이터를 불러옴
-  useEffect(
-    () => async () => {
-      if (realEndRef.current) return;
-      setIsLoading(true);
-      const newPage =
-        typeof fetcher === "function"
-          ? await fetcher({ page })
-          : await fetcher[listStatus]({ page });
-      if (newPage.success !== true) {
-        setIsEnd(true);
-        ToastMaker({
-          type: "error",
-          children: "예약 정보를 불러오지 못했어요."
-        });
-        return;
-      }
-
-      setIsEnd(newPage.data.lastPage);
-      setReservationList(prev => {
-        if (newPage.data.list.length !== 0)
-          return prev.concat(newPage.data.list);
-        return prev;
+  const runFetcher = async () => {
+    const fetcherIndex = () => listStatus;
+    const newPage =
+      typeof fetcher === "function"
+        ? await fetcher({ page: page.current })
+        : await fetcher[fetcherIndex()]({ page: page.current });
+    if (newPage.success !== true) {
+      setIsEnd(true);
+      ToastMaker({
+        type: "error",
+        children: "예약 정보를 불러오지 못했어요."
       });
-      setIsLoading(false);
-    },
-    [page]
-  );
+      return;
+    }
+
+    setIsEnd(newPage.data.lastPage);
+    setReservationList(prev => {
+      if (newPage.data.list.length !== 0) return prev.concat(newPage.data.list);
+      return prev;
+    });
+  };
+
+  useEffect(() => {
+    (async () => {
+      setReservationList([]);
+
+      page.current = 0;
+      realEndRef.current = false;
+      setIsEnd(false);
+
+      isLoading.current = true;
+      (async () => {
+        await runFetcher();
+      })();
+      isLoading.current = false;
+    })();
+  }, [listStatus]);
 
   useEffect(() => {
     if (isEnd) {
-      setIsLoading(true);
+      isLoading.current = true;
       disconnect();
       endRef.current = null;
       realEndRef.current = true;
@@ -119,18 +144,12 @@ const InfiniteList = ({ fetcher, baseURL, listStatus = null }) => {
   }, [isEnd]);
 
   useEffect(() => {
-    if (realEndRef.current) return;
-    if (isLoading) unobserve(endRef.current);
-    else observe(endRef.current);
-  }, [isLoading]);
-
-  useEffect(() => {
-    if (realEndRef.current) return setIsEnd(false);
-    setIsLoading(false);
+    if (realEndRef.current) return setIsEnd(() => false);
+    isLoading.current = false;
     return () => {
       if (endRef.current) observe(endRef.current);
       setReservationList([]);
-      setIsEnd(false);
+      setIsEnd(() => false);
     };
   }, []);
 
