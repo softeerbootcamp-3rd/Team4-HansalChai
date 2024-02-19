f645aadcb178d85b9af8d0f7d4ad34f362942085
Pransinia <trullerster@gmail.com>
2024-02-20 00:32:52 +0900

    feature: 변경된 리스트에 맞게 인자 추가 및 상단에 헤더와 탭바 고정

--- haul-fe/src/views/Check/List/Check.jsx
+++ haul-fe/src/views/Check/List/Check.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Header from "../../../components/Header/Header.jsx";
 import Margin from "../../../components/Margin/Margin.jsx";
 import MobileLayout from "../../../components/MobileLayout/MobileLayout.jsx";
@@ -7,7 +8,24 @@ import Typography from "../../../components/Typhography/Typhography.jsx";
 import InfiniteList from "./components/InfiniteList.jsx";
 import UnderBar from "../../../components/UnderBar/UnderBar.jsx";
 import TabBar from "./components/TabBar.jsx";
-import { useState } from "react";
+import EmptyListHolder from "./components/EmptyListHolder.jsx";
+import styled from "styled-components";
+
+const Floater = styled.div`
+  overflow: visible;
+  height: fit-content;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  z-index: 10;
+  background-color: white;
+`;
+
+const HorizontalLine = styled(UnderBar)`
+  width: 100%;
+  left: -20px;
+  position: relative;
+`;
 
 //TODO: API가 운송상태를 구별하여 가져올 수 있게 변경된 후 리스트 수정할 것!
 const Check = () => {
@@ -17,20 +35,25 @@ const Check = () => {
 
   return (
     <MobileLayout>
-      <Header home={false} back={false}>
-        <Typography font={"semiBold24"} color={"mainColor"}>
-          예약 확인
-        </Typography>
-      </Header>
-      <Margin height="32px" />
-      <TabBar
-        tabBarList={statusList}
-        setSelected={setSelectedStatus}
-        selected={selectedStatus}
+      <Floater>
+        <Header home={false} back={false}>
+          <Typography font={"semiBold24"} color={"mainColor"}>
+            예약 확인
+          </Typography>
+        </Header>
+        <Margin height="32px" />
+        <TabBar
+          tabBarList={statusList}
+          setSelected={setSelectedStatus}
+          selected={selectedStatus}
+        />
+        <HorizontalLine />
+      </Floater>
+      <Margin height="110px" />
+      <InfiniteList
+        listStatus={selectedStatus}
+        emptyListView={EmptyListHolder(selectedStatus)}
       />
-      <UnderBar />
-      <Margin height="20px" />
-      <InfiniteList />
       <Flex kind="flexColumn"></Flex>
       <NavigationBar selected="check" />
     </MobileLayout>
