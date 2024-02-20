9912d0a1d5a0971a713a88eb58dd5d99a9fb0654
sjmjys954646 <sjmjys954646@gmail.com>
2024-02-20 11:14:32 +0900

    feat : api code -> int
    데이터 코드를 int형으로 바꿈

--- haul-be/src/main/java/com/hansalchai/haul/common/utils/ApiResponse.java
+++ haul-be/src/main/java/com/hansalchai/haul/common/utils/ApiResponse.java
@@ -5,13 +5,13 @@ import lombok.Getter;
 @Getter
 public class ApiResponse<T> {
 	private int status;
-	private String code;
+	private int code;
 	private String message;
 	private T data;
 
-	private static final String SUCCESS_CODE = "200";
+	private static final int SUCCESS_CODE = 200;
 
-	private ApiResponse(int status, String code, String message, T data){
+	private ApiResponse(int status, int code, String message, T data){
 		this.status = status;
 		this.code = code;
 		this.message = message;
@@ -22,7 +22,7 @@ public class ApiResponse<T> {
 		return new ApiResponse<>(status, SUCCESS_CODE, message, data);
 	}
 
-	private static <T> ApiResponse<T> ApiResponseError(int status, String code ,String message) {
+	private static <T> ApiResponse<T> ApiResponseError(int status, int code ,String message) {
 		return new ApiResponse<>(status, code, message, null);
 	}
 
