588ad6cd4239e31ec4e258a7d63602c6b775dc4e
Pransinia <trullerster@gmail.com>
2024-02-20 21:57:55 +0900

    delete: 필요없는 주석 삭제

--- haul-fe/src/repository/checkRepository.js
+++ haul-fe/src/repository/checkRepository.js
@@ -1,9 +1,7 @@
-import ToastMaker from "../components/Toast/ToastMaker";
 import { ErrorMessageMap } from "../data/GlobalVariable";
 import { getAccessToken } from "../utils/localStorage";
 const apiKey = import.meta.env.VITE_API_KEY;
 
-//code 1103 예약 정보 없음
 export async function getGuestSummaryList({ reservationSerial }) {
   try {
     const response = await fetch(
@@ -66,8 +64,6 @@ export async function getUserSummaryList({ page, keyword = "매칭 중" }) {
   }
 }
 
-//Question: 비회원 상세 조회 API로 회원 것에 접근하면? 그 반대는?
-//code 1103 예약 정보 없음
 export async function getGuestReservationDetails({ reservationID }) {
   try {
     const response = await fetch(
@@ -99,11 +95,6 @@ export async function getGuestReservationDetails({ reservationID }) {
   }
 }
 
-//code 1002 리소스 접근 권한 없음 -> 이거 왜 200???
-//code 1103 예약 정보 없음 -> 이거 왜 200???
-//code 2001 헤더에 토큰이 없음 - 로그아웃
-//code 2002 토큰 유효성 검증 실패 - 로그아웃
-//code 2003 토큰이 만료됨 - 로그아웃
 export async function getUserReservationDetails({ reservationID }) {
   try {
     const response = await fetch(
