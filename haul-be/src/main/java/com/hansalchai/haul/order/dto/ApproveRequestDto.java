d5053ed449f77a2d5b85040c67182f2def9cc191
jiaeYoon <yoon529@sookmyung.ac.kr>
2024-02-22 02:14:33 +0900

    fix : ApproveRequestDto에 기본 생성자 추가
    역직렬화할 때 필요한 기본 생성자가 없어 HttpMessageNotReadableException: JSON parse error: Cannot construct instance 에러가 발생했다. dto에 @NoArgsConstructor를 붙여 해결했다.

--- haul-be/src/main/java/com/hansalchai/haul/order/dto/ApproveRequestDto.java
+++ haul-be/src/main/java/com/hansalchai/haul/order/dto/ApproveRequestDto.java
@@ -3,8 +3,10 @@ package com.hansalchai.haul.order.dto;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class ApproveRequestDto {
 
 	@NotNull(message = "예약 id는 null일 수 없다.")
