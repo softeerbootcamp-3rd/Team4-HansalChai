62c23ec41c6c7683328c1dd075d4f428253653c4
jiaeYoon <yoon529@sookmyung.ac.kr>
2024-02-21 10:51:31 +0900

    add : 필터 검증 제외 url 리스트에 고객/기사 로그인 url 추가
    고객/기사 로그인은 필터 검증을 하지 않도록 리스트에 url을 추가했다.

--- haul-be/src/main/java/com/hansalchai/haul/common/auth/config/JwtValidationFilter.java
+++ haul-be/src/main/java/com/hansalchai/haul/common/auth/config/JwtValidationFilter.java
@@ -32,7 +32,9 @@ public class JwtValidationFilter implements Filter {
 	private final ObjectMapper objectMapper;
 
 	private final String[] whiteListUris
-		= new String[] {"/api/v1/users/sign-in", "/api/v1/users/sign-up", "/auth/refresh/token", "*/h2-console*", "/swagger-ui/**", "*/api-docs*", "/api/v1/reservations/guest*"};
+		= new String[] {"/api/v1/users/sign-in", "/api/v1/users/sign-up", "/auth/refresh/token",
+		"/api/v2/users/customers/sign-in", "/api/v2/users/drivers/sign-in",
+		"*/h2-console*", "/swagger-ui/**", "*/api-docs*", "/api/v1/reservations/guest*"};
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws
