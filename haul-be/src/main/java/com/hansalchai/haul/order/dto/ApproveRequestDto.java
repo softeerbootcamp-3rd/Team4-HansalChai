bc5c9c64d860b7a134e50d90f4e635ab0f4c33f1
jiaeYoon <yoon529@sookmyung.ac.kr>
2024-02-21 23:43:41 +0900

    test : 오더 승인 단위 테스트
    오더 승인 성공, 실패 케이스에 대한 테스트 코드를 작성함

--- haul-be/src/main/java/com/hansalchai/haul/order/dto/ApproveRequestDto.java
+++ haul-be/src/main/java/com/hansalchai/haul/order/dto/ApproveRequestDto.java
@@ -1,6 +1,7 @@
 package com.hansalchai.haul.order.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
@@ -8,4 +9,9 @@ public class ApproveRequestDto {
 
 	@NotNull(message = "예약 id는 null일 수 없다.")
 	private Long id;
+
+	@Builder
+	public ApproveRequestDto(Long id) {
+		this.id = id;
+	}
 }
