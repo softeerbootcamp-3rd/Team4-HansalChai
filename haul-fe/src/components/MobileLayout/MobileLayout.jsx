7b74779ef1aafb5dd2ed2bcb2e6535e6a76ea492
Juicycool <100525337+sean2337@users.noreply.github.com>
2024-02-22 14:35:06 +0900

    hotfix: 모바일 layout 높이 조절
    모바일 layout 높이 조절하였습니다.

--- haul-fe/src/components/MobileLayout/MobileLayout.jsx
+++ haul-fe/src/components/MobileLayout/MobileLayout.jsx
@@ -14,7 +14,7 @@ const Mobile = styled.div`
   width: ${MaxDeviceWidth};
   min-width: ${({ minWidth }) => minWidth ?? MinDeviceWidth};
   min-height: 800px;
-  height: auto;
+  height: 100vh;
   margin: 0px auto;
   padding: 0px 20px;
   ${props => props.theme.flex.flexColumn};
