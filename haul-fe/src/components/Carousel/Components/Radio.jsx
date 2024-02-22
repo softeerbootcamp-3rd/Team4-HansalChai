0194bc69b4654f1021aaef3bd3098423a802b00d
Pransinia <trullerster@gmail.com>
2024-02-22 23:28:20 +0900

    refactor: Carousel의 구조 변경 및 이동 속도 증가
    라디오 버튼과 아이템 리스트를 컴포넌트로 분리
    내부 컴포넌트를 아래로 내림
    기존의 0.5초였던 이동시간을 0.2초로 변경

---
+++ haul-fe/src/components/Carousel/Components/Radio.jsx
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+const CarouselRadio = ({ carouselList, currentIndex, handleSwipe }) => {
+  return (
+    <RadioContainer>
+      {carouselList?.map((notUsedItem, carouselIndex) => {
+        return (
+          <input
+            key={`radio${carouselIndex}`}
+            type="radio"
+            name="carousel"
+            id={`radio${carouselIndex}`}
+            value={carouselIndex + 1}
+            checked={carouselIndex === currentIndex % carouselList.length}
+            onChange={() => {
+              handleSwipe(carouselIndex - (currentIndex % carouselList.length));
+            }}
+          />
+        );
+      })}
+    </RadioContainer>
+  );
+};
+
+const RadioContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  & input[type="radio"] {
+    background-color: ${({ theme }) => theme.colors.radioUnchecked};
+    width: 10px;
+    height: 10px;
+    &:checked {
+      width: 10px;
+      height: 10px;
+      background-color: ${({ theme }) => theme.colors.mainColor};
+    }
+  }
+`;
+export default CarouselRadio;
