bf0ac3897811a2eb650df3f1aa3dbd7629103401
sean2337 <sean2337@naver.com>
2024-02-23 01:58:04 +0900

    feat: vite-plugin-compression2 적용
    Vite 압축 플러그인 설치 및 적용하여 빠른 UX의 목적을 둔 최적화 작업입니다

--- haul-fe/vite.config.js
+++ haul-fe/vite.config.js
@@ -2,11 +2,16 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
+import { compression } from "vite-plugin-compression2";
 
 export default defineConfig({
   plugins: [
     react(),
     svgr(),
+    compression({
+      include: [/\.(js)$/, /\.(jsx)$/],
+      threshold: 1400
+    }),
     VitePWA({
       registerType: "autoUpdate",
       devOptions: {
