b8017adc0857d3317c52cda7930ea9798d1f741c
jiaeYoon <yoon529@sookmyung.ac.kr>
2024-02-22 21:36:02 +0900

    test : 오더 운송상태 변경 메서드의 단위 테스트

--- haul-be/src/test/java/com/hansalchai/haul/order/service/OrderServiceUnitTest.java
+++ haul-be/src/test/java/com/hansalchai/haul/order/service/OrderServiceUnitTest.java
@@ -25,12 +25,16 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 
 import com.hansalchai.haul.car.entity.Car;
+import com.hansalchai.haul.common.exceptions.BadRequestException;
 import com.hansalchai.haul.common.exceptions.ConflictException;
+import com.hansalchai.haul.common.exceptions.ForbiddenException;
 import com.hansalchai.haul.order.constants.OrderFilter;
 import com.hansalchai.haul.order.dto.ApproveRequestDto;
 import com.hansalchai.haul.order.dto.OrderSearchResponse;
+import com.hansalchai.haul.order.dto.TransportStatusChange;
 import com.hansalchai.haul.owner.entity.Owner;
 import com.hansalchai.haul.owner.repository.OwnerRepository;
+import com.hansalchai.haul.reservation.constants.TransportStatus;
 import com.hansalchai.haul.reservation.entity.Destination;
 import com.hansalchai.haul.reservation.entity.Reservation;
 import com.hansalchai.haul.reservation.entity.Source;
@@ -157,6 +161,120 @@ class OrderServiceUnitTest {
 			() -> orderService.approveV2(userId, new ApproveRequestDto(reservationId)));
 	}
 
+	@Test
+	@DisplayName("오더 운송상태 변경 가능 테스트 - 출발지에서 운송 전을 운송 중으로 변경하는 케이스")
+	void changeTransportStatusV2SuccessTest1() {
+
+		Long userId = 1L;
+		Long ownerId = 12L;
+		Long reservationId = 123L;
+		Reservation reservation = createReservationForChaneStatusV2Test(userId, ownerId, reservationId, NOT_STARTED);
+
+		List<Reservation> reservations = createReservationsForchangeTransportStatusV2(reservationId);
+		given(reservationRepository.findById(reservationId)).willReturn(Optional.of(reservation));
+		given(reservationRepository.findByDriverIdInProgress(userId)).willReturn(reservations);
+
+		//when
+		TransportStatusChange.RequestDtoV2 requestDto = TransportStatusChange.RequestDtoV2.builder()
+			.id(reservationId)
+			.latitude(37.5138649)  //학동 에티버스
+			.longitude(127.0295296)
+			.build();
+		TransportStatusChange.ResponseDtoV2 responseDto = orderService.changeTransportStatusV2(userId, requestDto);
+
+		//then
+		assertThat(responseDto.isDriverNearBy()).isTrue();
+		assertThat(responseDto.isHasInProgressOrder()).isFalse();
+	}
+
+	@Test
+	@DisplayName("오더 운송상태 변경 불가능 테스트 - 현위치가 출발지/목적지 300m 안이 아니면 운송상태를 변경할 수 없다")
+	void changeTransportStatusV2SuccessTest2() {
+
+		Long userId = 1L;
+		Long ownerId = 12L;
+		Long reservationId = 123L;
+		Reservation reservation = createReservationForChaneStatusV2Test(userId, ownerId, reservationId, NOT_STARTED);
+		given(reservationRepository.findById(reservationId)).willReturn(Optional.of(reservation));
+
+		//when
+		TransportStatusChange.RequestDtoV2 requestDto = TransportStatusChange.RequestDtoV2.builder()
+			.id(reservationId)
+			.latitude(37.445620228619)  //인천 미추홀구
+			.longitude(126.65182310263)
+			.build();
+		TransportStatusChange.ResponseDtoV2 responseDto = orderService.changeTransportStatusV2(userId, requestDto);
+
+		//then
+		assertThat(responseDto.isDriverNearBy()).isFalse();
+	}
+
+	@Test
+	@DisplayName("오더 운송상태 변경 불가능 테스트 - 진행 중인 오더가 있으면 새로운 오더를 동시에 진행할 수 없으므로 운송 상태를 변경할 수 없다")
+	void changeTransportStatusV2SuccessTest3() {
+
+		Long userId = 1L;
+		Long ownerId = 12L;
+		Long reservationId = 123L;
+		Reservation reservation = createReservationForChaneStatusV2Test(userId, ownerId, reservationId, NOT_STARTED);
+
+		TransportStatusChange.RequestDtoV2 requestDto = TransportStatusChange.RequestDtoV2.builder()
+			.id(reservationId)
+			.build();
+
+		List<Reservation> reservations = createReservationsForchangeTransportStatusV2SuccessTest3(reservationId);
+		given(reservationRepository.findById(reservationId)).willReturn(Optional.of(reservation));
+		given(reservationRepository.findByDriverIdInProgress(userId)).willReturn(reservations);
+
+		//when
+		TransportStatusChange.ResponseDtoV2 responseDto = orderService.changeTransportStatusV2(userId, requestDto);
+
+		//then
+		assertThat(responseDto.isHasInProgressOrder()).isTrue();
+	}
+
+	@Test
+	@DisplayName("오더 운송상태 변경 실패 테스트 - 오더 담당 기사가 아닌데 상태 변경을 요청하면 Forbidden 예외 발생")
+	void changeTransportStatusV2FailTest1() {
+
+		Long userId = 1L;
+		Long ownerId = 12L;
+		Long reservationId = 123L;
+		Reservation reservation = createReservationForChaneStatusV2Test(userId, ownerId, reservationId, NOT_STARTED);
+
+		TransportStatusChange.RequestDtoV2 requestDto = TransportStatusChange.RequestDtoV2.builder()
+			.id(reservationId)
+			.build();
+
+		given(reservationRepository.findById(reservationId)).willReturn(Optional.of(reservation));
+
+		//when, then
+		Long maliciousUserId = 44L;
+		assertThrows(ForbiddenException.class,
+			() -> orderService.changeTransportStatusV2(maliciousUserId, requestDto));
+	}
+
+	@Test
+	@DisplayName("오더 운송상태 변경 실패 테스트 - 운송 완료된 오더의 상태를 변경하려고 하면 Bad Request 예외 발생")
+	void changeTransportStatusV2FailTest2() {
+
+		Long userId = 1L;
+		Long ownerId = 12L;
+		Long reservationId = 123L;
+		Reservation reservation = createReservationForChaneStatusV2Test(userId, ownerId, reservationId, DONE);
+
+		TransportStatusChange.RequestDtoV2 requestDto = TransportStatusChange.RequestDtoV2.builder()
+			.id(reservationId)
+			.build();
+
+		given(reservationRepository.findById(reservationId)).willReturn(Optional.of(reservation));
+
+		//when, then
+		assertThrows(BadRequestException.class,
+			() -> orderService.changeTransportStatusV2(userId, requestDto));
+	}
+
+
 	/* ---------------- 예약 데이터 생성 메서드 ---------------- */
 	private Page<Reservation> createReservationsForFindAllTest() {
 
@@ -240,5 +358,35 @@ class OrderServiceUnitTest {
 		reservations.add(reservation1);
 		reservations.add(reservation2);
 		return reservations;
+	}
+
+	private static Reservation createReservationForChaneStatusV2Test(Long userId, Long ownerId, Long reservationId,
+		TransportStatus transportStatus) {
+		Users user = Users.builder().userId(userId).build();
+		Owner owner = Owner.builder().ownerId(ownerId).user(user).build();
+		Transport transport = Transport.builder().transportStatus(transportStatus).build();
+		Source source = Source.builder().latitude(37.51410107).longitude(127.03123824).build(); //학동역
+
+		return Reservation.builder()
+			.reservationId(reservationId)
+			.owner(owner)
+			.transport(transport)
+			.source(source)
+			.build();
+	}
+
+	private List<Reservation> createReservationsForchangeTransportStatusV2(Long reservationId) {
+		Reservation reservation1 = Reservation.builder().reservationId(reservationId).build();
+		Reservation reservation2 = Reservation.builder().reservationId(reservationId).build();
+		Reservation reservation3 = Reservation.builder().reservationId(reservationId).build();
+		return List.of(reservation1, reservation2, reservation3);
+	}
+
+	private List<Reservation> createReservationsForchangeTransportStatusV2SuccessTest3(Long reservationId) {
+		Long inProgressReservationId = 124L;
+		Reservation reservation1 = Reservation.builder().reservationId(reservationId).build();
+		Reservation reservation2 = Reservation.builder().reservationId(reservationId).build();
+		Reservation reservation3 = Reservation.builder().reservationId(inProgressReservationId).build();
+		return List.of(reservation1, reservation2, reservation3);
 	}
 }
