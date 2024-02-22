ca5cb1870ad28b408f7ca7bb960c1fd0ad41341c
Pransinia <trullerster@gmail.com>
2024-02-22 22:52:46 +0900

    refactor: CarInfoBox의 내부 컴포넌트 코드를 아래로 내림

--- haul-fe/src/components/CarInfoBox/CarInfoBox.jsx
+++ haul-fe/src/components/CarInfoBox/CarInfoBox.jsx
@@ -10,6 +10,44 @@ const CarInfoDescription = {
   after: "이런 차량을 제공해드렸어요."
 };
 
+const CarInfoBox = ({ phase, type, capacity, volumn, photo, quantity = 1 }) => {
+  return (
+    <CarInfoFrame>
+      <Typography font={"semiBold16"}>{CarInfoDescription[phase]}</Typography>
+      <Margin height="8px" />
+      <UnderBar />
+      <Margin height="8px" />
+      <CarCard>
+        <QuantityBox>
+          {quantity !== 1 ? (
+            <Typography font={"semiBold20"}>{`x${quantity}`}</Typography>
+          ) : (
+            <></>
+          )}
+        </QuantityBox>
+        <CarCardImage type={type} src={photo} />
+      </CarCard>
+      <Margin height="12px" />
+      <DescriptionTextArea>
+        <Typography font={"semiBold16"}>차종</Typography>
+        <Typography font={"medium16"}>{type}</Typography>
+      </DescriptionTextArea>
+      <Margin height="12px" />
+      <UnderBar />
+      <Margin height="12px" />
+      <DescriptionTextArea>
+        <Typography font={"semiBold16"}>적재량</Typography>
+        <Typography font={"medium16"}>{capacity}</Typography>
+      </DescriptionTextArea>
+      <Margin height="12px" />
+      <DescriptionTextArea>
+        <Typography font={"semiBold16"}>최대 너비,길이,높이</Typography>
+        <Typography font={"medium16"}>{volumn} M</Typography>
+      </DescriptionTextArea>
+    </CarInfoFrame>
+  );
+};
+
 const CarInfoFrame = styled.div`
   width: 100%;
   ${({ theme }) => theme.flex.flexColumn};
@@ -49,43 +87,5 @@ const DescriptionTextArea = styled.div`
   width: 100%;
   ${({ theme }) => theme.flex.flexBetween};
 `;
-
-const CarInfoBox = ({ phase, type, capacity, volumn, photo, quantity = 1 }) => {
-  return (
-    <CarInfoFrame>
-      <Typography font={"semiBold16"}>{CarInfoDescription[phase]}</Typography>
-      <Margin height="8px" />
-      <UnderBar />
-      <Margin height="8px" />
-      <CarCard>
-        <QuantityBox>
-          {quantity !== 1 ? (
-            <Typography font={"semiBold20"}>{`x${quantity}`}</Typography>
-          ) : (
-            ""
-          )}
-        </QuantityBox>
-        <CarCardImage type={type} src={photo} />
-      </CarCard>
-      <Margin height="12px" />
-      <DescriptionTextArea>
-        <Typography font={"semiBold16"}>차종</Typography>
-        <Typography font={"medium16"}>{type}</Typography>
-      </DescriptionTextArea>
-      <Margin height="12px" />
-      <UnderBar />
-      <Margin height="12px" />
-      <DescriptionTextArea>
-        <Typography font={"semiBold16"}>적재량</Typography>
-        <Typography font={"medium16"}>{capacity}</Typography>
-      </DescriptionTextArea>
-      <Margin height="12px" />
-      <DescriptionTextArea>
-        <Typography font={"semiBold16"}>최대 너비,길이,높이</Typography>
-        <Typography font={"medium16"}>{volumn} M</Typography>
-      </DescriptionTextArea>
-    </CarInfoFrame>
-  );
-};
 
 export default CarInfoBox;
