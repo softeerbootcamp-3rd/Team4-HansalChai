d1fa6d61b6d12eda7032e15e5c188a2b57c7fabf
Pransinia <trullerster@gmail.com>
2024-02-24 09:28:00 +0900

    move: app에 있던 설치 모달을 component로 분리
    추가로 애니메이션 적용

--- haul-fe/src/App.jsx
+++ haul-fe/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import { ThemeProvider } from "styled-components";
 import "./assets/fonts/fonts.css";
 import Router from "./routes/Router.jsx";
@@ -7,55 +6,8 @@ import theme from "./styles/theme/Theme.jsx";
 import ToastRoot from "./components/Toast/ToastRoot.jsx";
 import { ReservationStoreProvider } from "./store/reservationStore.jsx";
 import MobileInstallPrompt from "./components/AppInstallPrompt/MobileInstallPrompt.jsx";
-import ToastMaker from "./components/Toast/ToastMaker.jsx";
-import { getNotInstall, setNotInstall } from "./utils/localStorage.js";
 
 function App() {
-  const [showInstallModal, setShowInstallModal] = useState(false);
-
-  useEffect(() => {
-    if (getNotInstall() === "true") return;
-    const isDeviceIOS = /iPad|iPhone|iPod/.test(window.navigator.userAgent);
-    if (isDeviceIOS) {
-      setShowInstallModal(true);
-      return;
-    }
-    const before = event => {
-      event.preventDefault();
-      window.diferredPrompt = event;
-      setShowInstallModal(true);
-      console.log("beforeinstallprompt run!");
-    };
-    window.addEventListener("beforeinstallprompt", before);
-
-    const installed = event => {
-      console.log("👍", "appinstalled", event);
-      ToastMaker({ type: "success", message: "앱이 설치되었습니다." });
-      // Clear the deferredPrompt so it can be garbage collected
-      window.deferredPrompt = null;
-    };
-    window.addEventListener("appinstalled", installed);
-
-    return () => {
-      window.removeEventListener("beforeinstallprompt", before);
-      window.removeEventListener("appinstalled", installed);
-    };
-  }, []);
-
-  const handleInstall = () => {
-    console.log(window.diferredPrompt);
-    // PWA 설치 로직
-    window.diferredPrompt.prompt();
-    setShowInstallModal(false);
-    const result = window.diferredPrompt.userChoice;
-    console.log(result);
-    window.diferredPrompt = null;
-  };
-
-  const handleClose = () => {
-    setShowInstallModal(false);
-    setNotInstall();
-  };
   return (
     <ThemeProvider theme={theme}>
       <ReservationStoreProvider>
@@ -63,13 +15,7 @@ function App() {
         <Router />
         <ToastRoot />
       </ReservationStoreProvider>
-      {showInstallModal && (
-        <MobileInstallPrompt
-          handleInstallClick={handleInstall}
-          handleCancelClick={handleClose}
-          isOpen={showInstallModal}
-        />
-      )}
+      <MobileInstallPrompt />
     </ThemeProvider>
   );
 }
--- haul-fe/src/components/AppInstallPrompt/MobileInstallPrompt.jsx
+++ haul-fe/src/components/AppInstallPrompt/MobileInstallPrompt.jsx
@@ -1,20 +1,146 @@
-
+import { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 import Flex from "../Flex/Flex.jsx";
 import Margin from "../Margin/Margin.jsx";
+import ToastMaker from "../Toast/ToastMaker.jsx";
 import Typography from "../Typhography/Typhography.jsx";
 import TypographySpan from "../Typhography/TyphographySpan.jsx";
 import { MaxDeviceWidth } from "../../data/GlobalVariable.js";
+import { getDeviceInfo } from "../../utils/helper.js";
+import { getNotInstall, setNotInstall } from "../../utils/localStorage.js";
 
-const InstallPrompt = styled.dialog`
+const MobileInstallPrompt = () => {
+  const deviceInfo = useRef(getDeviceInfo());
+
+  const [showInstallModal, setShowInstallModal] = useState(true);
+  const modalRef = useRef();
+  const [manual, setManual] = useState(false);
+  const before = event => {
+    if (getNotInstall() === "true") return;
+    event.preventDefault();
+    window.diferredPrompt = event;
+    setShowInstallModal(true);
+  };
+
+  const installed = () => {
+    ToastMaker({ type: "success", message: "앱이 설치되었습니다." });
+    window.deferredPrompt = null;
+  };
+  //FIXME: 주석처리 했던 getNotInstall() === "true" 부분을 다시 주석처리 해제
+  useEffect(() => {
+    //if (getNotInstall() === "true") return;
+    if (
+      deviceInfo.current.device === "iOS" ||
+      (deviceInfo.current.device === "Android" &&
+        deviceInfo.current.browser === "firefox")
+    ) {
+      setManual(true);
+      setShowInstallModal(true);
+      return;
+    }
+    window.addEventListener("beforeinstallprompt", before);
+    window.addEventListener("appinstalled", installed);
+    return () => {
+      window.removeEventListener("beforeinstallprompt", before);
+      window.removeEventListener("appinstalled", installed);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log(modalRef);
+    modalRef.current.style.transform = showInstallModal
+      ? "translate(-50%, 0%)"
+      : "translate(-50%, 150%)";
+    console.log(modalRef.current.style.transform);
+  }, [showInstallModal]);
+
+  const handleInstall = async () => {
+    console.log(window.diferredPrompt);
+
+    // PWA 설치 로직
+    window.diferredPrompt.prompt();
+    setShowInstallModal(false);
+    const result = await window.diferredPrompt.userChoice;
+    console.log(result);
+    if (result.outcome === "dismissed") {
+      setNotInstall();
+      window.diferredPrompt = null;
+    }
+    window.diferredPrompt = null;
+  };
+
+  const handleClose = () => {
+    setShowInstallModal(false);
+    setNotInstall();
+  };
+
+  const NeedManual = () => {
+    let message = "";
+    if (deviceInfo.current.device === "iOS")
+      message = "iOS에서는 홈 화면에 추가 버튼을 눌러 설치할 수 있어요.";
+    else if (
+      deviceInfo.current.device === "Android" &&
+      deviceInfo.current.browser === "firefox"
+    )
+      message =
+        "Firefox에서는 브라우저의 메뉴에서 설치 버튼을 눌러 설치할 수 있어요.";
+    return (
+      <CustomFlex kind="flexColumnCenter">
+        <Typography font="semiBold14">{message}</Typography>
+        <Margin height="20px" />
+        <PromptButton onClick={handleClose} role="cancel" width="70px">
+          확인
+        </PromptButton>
+      </CustomFlex>
+    );
+  };
+
+  return (
+    <InstallPrompt ref={modalRef}>
+      <Margin height="20px" />
+      <CustomFlex kind="flexBetweenCenter" padding={"0 20px"}>
+        <IconImage src="/icon_x192.png" width={72} alt="icon" />
+        <Typography color="black" font="bold20">
+          <TypographySpan color="mainColor" font="semiBold20">
+            Haul
+          </TypographySpan>
+          을 설치하시겠어요?
+        </Typography>
+      </CustomFlex>
+      <Margin height="20px" />
+      {manual ? (
+        <NeedManual />
+      ) : (
+        <CustomFlex kind="flexBetweenCenter" padding={"0 20px"} gap={"10px"}>
+          <PromptButton
+            id={"installButton"}
+            onClick={handleInstall}
+            role="install"
+          >
+            설치할래요
+          </PromptButton>
+          <PromptButton onClick={handleClose} role="cancel">
+            웹으로 볼래요
+          </PromptButton>
+        </CustomFlex>
+      )}
+      <Margin height="20px" />
+    </InstallPrompt>
+  );
+};
+
+export default MobileInstallPrompt;
+
+const InstallPrompt = styled.div`
   position: fixed;
-  bottom: 0;
+  bottom: 0%;
   left: 50%;
-  transform: translateX(-50%);
   width: 100%;
+  transform: translate(-50%, 150%);
+  transition: transform 0.3s;
   max-width: ${MaxDeviceWidth};
   border-radius: 16px 16px 0 0;
-  border: 1px solid #000;
+  border: 0px solid #000;
   background-color: #fff;
   z-index: 100;
   box-shadow: 0 -10px 10px 1px rgba(0, 0, 0, 0.1);
@@ -28,11 +154,11 @@ const IconImage = styled.img`
 
 const CustomFlex = styled(Flex)`
   padding: ${({ padding }) => padding};
-  gap: ${({gap}) => gap ?? "unset"};
+  gap: ${({ gap }) => gap ?? "unset"};
 `;
 
 const PromptButton = styled.button`
-  width: 100%;
+  width: ${({ width }) => width ?? "100%"};
   padding: 10px;
   border: none;
   border-radius: 8px;
@@ -41,54 +167,3 @@ const PromptButton = styled.button`
     theme.colors[role === "install" ? "mainColor" : "subColor"]};
   color: #fff;
 `;
-
-const MobileInstallPrompt = ({
-  handleInstallClick,
-  handleCancelClick,
-  isOpen
-}) => {
-  const isDeviceIOS = /iPad|iPhone|iPod/.test(window.navigator.userAgent);
-
-  return (
-    <InstallPrompt open={isOpen}>
-      <Margin height="20px" />
-      <CustomFlex kind="flexBetweenCenter" padding={"0 20px"}>
-        <IconImage src="/icon_x192.png" width={72} alt="icon" />
-        <Typography color="black" font="bold20">
-          <TypographySpan color="mainColor" font="semiBold20">
-            Haul
-          </TypographySpan>
-          을 설치하시겠어요?
-        </Typography>
-      </CustomFlex>
-      <Margin height="20px" />
-      {isDeviceIOS ? (
-        <CustomFlex kind="flexColumnCenter">
-          <Typography font="semiBold14">
-            IOS에서는 홈 화면에 추가 버튼을 눌러 설치할 수 있어요.
-          </Typography>
-          <Margin height="20px" />
-          <PromptButton onClick={handleCancelClick} role="cancel">
-            확인
-          </PromptButton>
-        </CustomFlex>
-      ) : (
-        <CustomFlex kind="flexBetweenCenter" padding={"0 20px"} gap={"10px"}>
-          <PromptButton
-            id={"installButton"}
-            onClick={handleInstallClick}
-            role="install"
-          >
-            설치할래요
-          </PromptButton>
-          <PromptButton onClick={handleCancelClick} role="cancel">
-            웹으로 볼래요
-          </PromptButton>
-        </CustomFlex>
-      )}
-      <Margin height="20px" />
-    </InstallPrompt>
-  )
-}
-
-export default MobileInstallPrompt;
