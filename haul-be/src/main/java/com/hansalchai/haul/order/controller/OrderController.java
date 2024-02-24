58aab75836d9641812ad3d2ffb801c1445e467e1
jiaeYoon <yoon529@sookmyung.ac.kr>
2024-02-24 17:59:52 +0900

    refactor : @LoggedInUser 어노테이션 적용
    @LoggedInUser 어노테이션으로 기존의 HttpServletRequest로부터 유저 정보를 추출하는 중복 코드를 제거했다.

--- haul-be/src/main/java/com/hansalchai/haul/order/controller/OrderController.java
+++ haul-be/src/main/java/com/hansalchai/haul/order/controller/OrderController.java
@@ -1,6 +1,5 @@
 package com.hansalchai.haul.order.controller;
 
-import static com.hansalchai.haul.common.auth.jwt.JwtProvider.*;
 import static com.hansalchai.haul.common.utils.ApiResponse.*;
 import static com.hansalchai.haul.common.utils.SuccessCode.*;
 
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hansalchai.haul.common.auth.anotation.LoggedInUser;
 import com.hansalchai.haul.common.auth.dto.AuthenticatedUser;
 import com.hansalchai.haul.common.utils.ApiResponse;
 import com.hansalchai.haul.order.dto.ApproveRequestDto;
@@ -21,7 +21,6 @@ import com.hansalchai.haul.order.dto.OrderSearchResponse;
 import com.hansalchai.haul.order.dto.TransportStatusChange;
 import com.hansalchai.haul.order.service.OrderService;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
@@ -36,82 +35,80 @@ public class OrderController {
 
 	@GetMapping(V1_ORDERS_PATH)
 	public ResponseEntity<ApiResponse<OrderSearchResponse>> findAll(
-			HttpServletRequest request,
+			@LoggedInUser AuthenticatedUser authenticatedUser,
 			@RequestParam(value = "sort", defaultValue = "default") String sort,
 			@PositiveOrZero @RequestParam(value = "page", defaultValue = "0") int page) {
-		AuthenticatedUser auth = (AuthenticatedUser)request.getAttribute(AUTHENTICATE_USER);
-		OrderSearchResponse orders = orderService.findAll(auth.getUserId(), sort, page);
+		OrderSearchResponse orders = orderService.findAll(authenticatedUser.getUserId(), sort, page);
 		return ResponseEntity.ok(success(GET_SUCCESS, orders));
 	}
 
 	@GetMapping(V1_ORDERS_PATH + "/{id}")
 	public ResponseEntity<ApiResponse<OrderDetailDTO>> findById(
-		@PathVariable("id") Long id,
-		HttpServletRequest request) {
-		AuthenticatedUser auth = (AuthenticatedUser)request.getAttribute(AUTHENTICATE_USER);
+			@PathVariable("id") Long id) {
 		OrderDetailDTO response = orderService.getOrderDetail(id);
 		return ResponseEntity.ok(success(GET_SUCCESS, response));
 	}
 
 	@PatchMapping(V1_ORDERS_PATH + "/approve")
-	public ResponseEntity<ApiResponse<Object>> approveOrder(HttpServletRequest request,
-		@Valid @RequestBody ApproveRequestDto approveRequestDto) {
-
-		AuthenticatedUser auth = (AuthenticatedUser)request.getAttribute(AUTHENTICATE_USER);
-		orderService.approve(auth.getUserId(), approveRequestDto);
+	public ResponseEntity<ApiResponse<Object>> approveOrder(
+			@LoggedInUser AuthenticatedUser authenticatedUser,
+			@Valid @RequestBody ApproveRequestDto approveRequestDto) {
+		orderService.approve(authenticatedUser.getUserId(), approveRequestDto);
 		return ResponseEntity.ok(success(GET_SUCCESS, null));
 	}
 
 	@GetMapping(V1_ORDERS_PATH + "/mine")
-	public ResponseEntity<ApiResponse<OrderDTO>> getMyOrder(@RequestParam(value = "keyword", defaultValue = "운송 전") String keyword, @RequestParam(value = "page", defaultValue = "0") int page, HttpServletRequest request){
-		AuthenticatedUser auth = (AuthenticatedUser)request.getAttribute(AUTHENTICATE_USER);
-		OrderDTO response = orderService.getOrder(keyword, page, auth.getUserId());
+	public ResponseEntity<ApiResponse<OrderDTO>> getMyOrder(
+			@LoggedInUser AuthenticatedUser authenticatedUser,
+			@RequestParam(value = "keyword", defaultValue = "운송 전") String keyword,
+			@RequestParam(value = "page", defaultValue = "0") int page){
+		OrderDTO response = orderService.getOrder(keyword, page, authenticatedUser.getUserId());
 		return ResponseEntity.ok(success(GET_SUCCESS, response));
 	}
 
 	@GetMapping(V1_ORDERS_PATH + "/mine/{id}")
-	public ResponseEntity<ApiResponse<OrderDetailDTO>> getMyOrderDetail(@PathVariable("id")Long id, HttpServletRequest request){
-		AuthenticatedUser auth = (AuthenticatedUser)request.getAttribute(AUTHENTICATE_USER);
-		OrderDetailDTO response = orderService.getOrderMineDetail(id, auth.getUserId());
+	public ResponseEntity<ApiResponse<OrderDetailDTO>> getMyOrderDetail(
+			@LoggedInUser AuthenticatedUser authenticatedUser,
+			@PathVariable("id")Long id){
+		OrderDetailDTO response = orderService.getOrderMineDetail(id, authenticatedUser.getUserId());
 		return ResponseEntity.ok(success(GET_SUCCESS, response));
 	}
 
 	@PatchMapping(V1_ORDERS_PATH + "/status")
 	public ResponseEntity<ApiResponse<TransportStatusChange.ResponseDto>> changeTransportStatus(
-			HttpServletRequest request,
+			@LoggedInUser AuthenticatedUser authenticatedUser,
 			@Valid @RequestBody TransportStatusChange.RequestDto requestDto) {
-		AuthenticatedUser auth = (AuthenticatedUser)request.getAttribute(AUTHENTICATE_USER);
-		TransportStatusChange.ResponseDto responseDto = orderService.changeTransportStatus(auth.getUserId(), requestDto);
+		TransportStatusChange.ResponseDto responseDto
+			= orderService.changeTransportStatus(authenticatedUser.getUserId(), requestDto);
 		return ResponseEntity.ok(success(GET_SUCCESS, responseDto));
 	}
 
 	@GetMapping(V2_ORDERS_PATH)
 	public ResponseEntity<ApiResponse<OrderSearchResponse>> findAllV2(
-		HttpServletRequest request,
-		@RequestParam(value = "sort", defaultValue = "default") String sort,
-		@PositiveOrZero @RequestParam(value = "page", defaultValue = "0") int page,
-		@RequestParam(value = "latitude") double latitude,
-		@RequestParam(value = "longitude") double longitude) {
-		AuthenticatedUser auth = (AuthenticatedUser)request.getAttribute(AUTHENTICATE_USER);
-		OrderSearchResponse orders = orderService.findAllV2(auth.getUserId(), sort, page, latitude, longitude );
+			@LoggedInUser AuthenticatedUser authenticatedUser,
+			@RequestParam(value = "sort", defaultValue = "default") String sort,
+			@PositiveOrZero @RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "latitude") double latitude,
+			@RequestParam(value = "longitude") double longitude) {
+		OrderSearchResponse orders
+			= orderService.findAllV2(authenticatedUser.getUserId(), sort, page, latitude, longitude);
 		return ResponseEntity.ok(success(GET_SUCCESS, orders));
 	}
 	
 	@PatchMapping(V2_ORDERS_PATH + "/approve")
 	public ResponseEntity<ApiResponse<Object>> approveOrderV2(
-			HttpServletRequest request,
+			@LoggedInUser AuthenticatedUser authenticatedUser,
 			@Valid @RequestBody ApproveRequestDto approveRequestDto) {
-		AuthenticatedUser auth = (AuthenticatedUser)request.getAttribute(AUTHENTICATE_USER);
-		orderService.approveV2(auth.getUserId(), approveRequestDto);
+		orderService.approveV2(authenticatedUser.getUserId(), approveRequestDto);
 		return ResponseEntity.ok(success(GET_SUCCESS, null));
 	}
 
 	@PatchMapping(V2_ORDERS_PATH + "/status")
 	public ResponseEntity<ApiResponse<TransportStatusChange.ResponseDtoV2>> changeTransportStatusV2(
-			HttpServletRequest request,
+			@LoggedInUser AuthenticatedUser authenticatedUser,
 			@Valid @RequestBody TransportStatusChange.RequestDtoV2 requestDto) {
-		AuthenticatedUser auth = (AuthenticatedUser)request.getAttribute(AUTHENTICATE_USER);
-		TransportStatusChange.ResponseDtoV2 responseDto = orderService.changeTransportStatusV2(auth.getUserId(), requestDto);
+		TransportStatusChange.ResponseDtoV2 responseDto
+			= orderService.changeTransportStatusV2(authenticatedUser.getUserId(), requestDto);
 		return ResponseEntity.ok(success(GET_SUCCESS, responseDto));
 	}
 }
--- haul-be/src/main/java/com/hansalchai/haul/reservation/controller/ReservationController.java
+++ haul-be/src/main/java/com/hansalchai/haul/reservation/controller/ReservationController.java
@@ -1,6 +1,5 @@
 package com.hansalchai.haul.reservation.controller;
 
-import static com.hansalchai.haul.common.auth.jwt.JwtProvider.*;
 import static com.hansalchai.haul.common.utils.ApiResponse.*;
 import static com.hansalchai.haul.reservation.dto.ReservationRequest.*;
 import static com.hansalchai.haul.reservation.dto.ReservationResponse.*;
@@ -15,12 +14,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hansalchai.haul.common.auth.anotation.LoggedInUser;
 import com.hansalchai.haul.common.auth.dto.AuthenticatedUser;
 import com.hansalchai.haul.common.utils.ApiResponse;
 import com.hansalchai.haul.common.utils.SuccessCode;
 import com.hansalchai.haul.reservation.service.ReservationService;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
@@ -32,68 +31,63 @@ public class ReservationController {
 
 	@PostMapping(V1_RESERVATIONS_PATH)
 	public ResponseEntity<ApiResponse<ReservationRecommendationDTO>> postCustomerReservation(
-		@Valid @RequestBody CreateReservationDTO reservationDTO,
-		HttpServletRequest request
-	) {
-		AuthenticatedUser auth = (AuthenticatedUser)request.getAttribute(AUTHENTICATE_USER);
-		ReservationRecommendationDTO response = reservationService.createReservation(reservationDTO, auth.getUserId());
+			@LoggedInUser AuthenticatedUser authenticatedUser,
+			@Valid @RequestBody CreateReservationDTO reservationDTO) {
+		ReservationRecommendationDTO response
+			= reservationService.createReservation(reservationDTO, authenticatedUser.getUserId());
 		return ResponseEntity.ok(success(SuccessCode.GET_SUCCESS, response));
 	}
 
 	@PatchMapping(V1_RESERVATIONS_PATH + "/{id}")
 	public ResponseEntity<ApiResponse<Object>> patchCustomerReservation(
-		@PathVariable("id") Long id,
-		HttpServletRequest request
-	) {
-		AuthenticatedUser auth = (AuthenticatedUser)request.getAttribute(AUTHENTICATE_USER);
-		reservationService.patchReservation(id, auth.getUserId());
+			@LoggedInUser AuthenticatedUser authenticatedUser,
+			@PathVariable("id") Long id) {
+		reservationService.patchReservation(id, authenticatedUser.getUserId());
 		return ResponseEntity.ok(success(SuccessCode.GET_SUCCESS, null));
 	}
 
 	@PostMapping(V1_RESERVATIONS_PATH + "/guest")
 	public ResponseEntity<ApiResponse<ReservationRecommendationDTO>> postGuestReservation(
-		@Valid @RequestBody CreateReservationGuestDTO reservationDTO
-	) {
+			@Valid @RequestBody CreateReservationGuestDTO reservationDTO) {
 		ReservationRecommendationDTO response = reservationService.createGuestReservation(reservationDTO);
 		return ResponseEntity.ok(success(SuccessCode.GET_SUCCESS, response));
 	}
 
 	@PatchMapping(V1_RESERVATIONS_PATH + "/guest/{id}")
 	public ResponseEntity<ApiResponse<Object>> patchGuestReservation(
-		@PathVariable("id") Long id
-	) {
+			@PathVariable("id") Long id) {
 		reservationService.patchGuestReservation(id);
 		return ResponseEntity.ok(success(SuccessCode.GET_SUCCESS, null));
 	}
 
 	@GetMapping(V1_RESERVATIONS_PATH)
 	public ResponseEntity<ApiResponse<ReservationDTO>> getCustomerReservation(
-		@RequestParam(value = "keyword", defaultValue = "매칭 중") String keyword,
-		@RequestParam(value = "page", defaultValue = "0") int page, HttpServletRequest request) {
-		AuthenticatedUser auth = (AuthenticatedUser)request.getAttribute(AUTHENTICATE_USER);
-		ReservationDTO response = reservationService.getReservation(keyword, page, auth.getUserId());
+			@LoggedInUser AuthenticatedUser authenticatedUser,
+			@RequestParam(value = "keyword", defaultValue = "매칭 중") String keyword,
+			@RequestParam(value = "page", defaultValue = "0") int page) {
+		ReservationDTO response = reservationService.getReservation(keyword, page, authenticatedUser.getUserId());
 		return ResponseEntity.ok(success(SuccessCode.GET_SUCCESS, response));
 	}
 
 	@GetMapping(V1_RESERVATIONS_PATH + "/guest")
 	public ResponseEntity<ApiResponse<ReservationDTO>> getGuestReservation(
-		@RequestParam(value = "number") String number) {
+			@RequestParam(value = "number") String number) {
 		ReservationDTO response = reservationService.getGuestReservation(number);
 		return ResponseEntity.ok(success(SuccessCode.GET_SUCCESS, response));
 	}
 
 	@GetMapping(V1_RESERVATIONS_PATH + "/{id}")
-	public ResponseEntity<ApiResponse<ReservationDetailDTO>> getCustomerReservationDetail(@PathVariable("id") Long id,
-		HttpServletRequest request) {
-		AuthenticatedUser auth = (AuthenticatedUser)request.getAttribute(AUTHENTICATE_USER);
-		ReservationDetailDTO response = reservationService.getReservationDetail(id, auth.getUserId());
+	public ResponseEntity<ApiResponse<ReservationDetailDTO>> getCustomerReservationDetail(
+			@LoggedInUser AuthenticatedUser authenticatedUser,
+			@PathVariable("id") Long id) {
+		ReservationDetailDTO response = reservationService.getReservationDetail(id, authenticatedUser.getUserId());
 		return ResponseEntity.ok(success(SuccessCode.GET_SUCCESS, response));
 	}
 
 	@GetMapping(V1_RESERVATIONS_PATH + "/guest/{id}")
-	public ResponseEntity<ApiResponse<ReservationDetailDTO>> getGuestReservationDetail(@PathVariable("id") Long id) {
+	public ResponseEntity<ApiResponse<ReservationDetailDTO>> getGuestReservationDetail(
+			@PathVariable("id") Long id) {
 		ReservationDetailDTO response = reservationService.getGuestReservationDetail(id);
 		return ResponseEntity.ok(success(SuccessCode.GET_SUCCESS, response));
 	}
-
 }
