7e17a952778a455503f8e2e8cd35f538b70ba3cf
sjmjys954646 <sjmjys954646@gmail.com>
2024-02-24 12:20:34 +0900

    feat : selectCategory Errorcode
    에러코드 처리

--- haul-be/src/main/java/com/hansalchai/haul/common/utils/CarCategorySelector.java
+++ haul-be/src/main/java/com/hansalchai/haul/common/utils/CarCategorySelector.java
@@ -1,13 +1,15 @@
 package com.hansalchai.haul.common.utils;
 
+import static com.hansalchai.haul.common.utils.ErrorCode.*;
+
 import com.hansalchai.haul.car.constants.CarCategory;
+import com.hansalchai.haul.common.exceptions.BadRequestException;
 import com.hansalchai.haul.reservation.entity.CargoOption;
 
 public class CarCategorySelector {
 	public static CarCategory selectCarCategory(CargoOption cargoOption){
-		//TODO 예외처리로 변경
 		if(cargoOption.isFrozen() && cargoOption.isRefrigerated())
-			return CarCategory.DEFAULT;
+			throw new BadRequestException(INVALID_STATUS);
 
 		if(cargoOption.isFrozen())
 			return CarCategory.IS_FREEZABLE;
