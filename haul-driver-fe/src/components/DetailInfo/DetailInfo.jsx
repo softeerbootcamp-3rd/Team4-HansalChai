7d19b3dfc2d71a57eb2d048335139b8bc62483a3
Pransinia <trullerster@gmail.com>
2024-02-27 15:37:38 +0900

    fix: 도착지 이름이 없으면 도착지 주소를 8px 올리도록 변경

--- haul-driver-fe/src/components/DetailInfo/DetailInfo.jsx
+++ haul-driver-fe/src/components/DetailInfo/DetailInfo.jsx
@@ -103,8 +103,8 @@ const DetailInfo = ({
                 {dstName}
               </Typography>
               <Margin height="4px" />
-
               <Typography font="semiBold12">{dstAddress}</Typography>
+              {!dstName && <Margin height="8px" />}
             </PlaceInfo>
           </Flex>
         </Flex>
